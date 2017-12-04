e59b38e54cd2fa0609cd8c5036aef8b7b5f232a6
Dunya Kirkali <dunyakirkali@gmail.com>
2017-12-04 23:28:16 +0100

    Prep day3

---
+++ 2017/day_3.rb
@@ -0,0 +1,7 @@
+require 'matrix'
+
+class Day3
+  def self.run(destination)
+    -10
+  end
+end
---
+++ 2017/spec/day_3_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+require_relative "../day_3"
+
+describe "Day 3" do
+  let(:day_3) { Day3 }
+
+  describe "puzzle" do
+    it "1st solution" do
+      expect(day_3.run(1)).to eq(0)
+    end
+
+    it "2nd solution" do
+      expect(day_3.run(12)).to eq(3)
+    end
+
+    it "3rd solution" do
+      expect(day_3.run(23)).to eq(2)
+    end
+
+    it "4th solution" do
+      expect(day_3.run(1024)).to eq(31)
+    end
+
+    it "D solution" do
+      expect(day_3.run(277678)).to eq(-1)
+    end
+  end
+end
