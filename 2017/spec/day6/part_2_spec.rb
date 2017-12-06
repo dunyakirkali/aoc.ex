60dd48c83b6b07a40c61a1bf2320a745c32ad196
Dunya Kirkali <dunya.kirkali@icemobile.com>
2017-12-06 09:07:58 +0100

    Clean up

--- 2017/spec/day6/part_2_spec.rb
+++ 2017/spec/day6/part_2_spec.rb
@@ -3,16 +3,16 @@ require "spec_helper"
 require_relative "../../day6/part_2.rb"
 
 describe "Day 6" do
-  let(:day_6) { Day6::Part2 }
+  let(:solver) { Day6::Part2 }
 
   describe "puzzle" do
     it "1st solution" do
-      expect(day_6.run([0, 2, 7, 0])).to eq(4)
+      expect(solver.run([0, 2, 7, 0])).to eq(4)
     end
 
     it "D solution" do
       lines = File.readlines('spec/day6/sequences').map(&:to_i)
-      expect(day_6.run(lines)).to eq(1695)
+      expect(solver.run(lines)).to eq(1695)
     end
   end
 end
