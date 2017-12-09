47f3bdd54a9acbc69bb0e4c1c31848eb905d140f
Dunya Kirkali <dunyakirkali@gmail.com>
2017-12-09 15:59:28 +0100

    Clean up Day 1

---
+++ 2017/day1/part_1.rb
@@ -0,0 +1,15 @@
+module Day1
+  class Part1
+    def self.run(s_input)
+      return 0 if s_input.length < 2
+
+      prev_digit = s_input[s_input.length - 1]
+      sum = 0
+      s_input.chars.each_with_index do |curr_digit, index|
+        sum += curr_digit.to_i if prev_digit == curr_digit
+        prev_digit = curr_digit
+      end
+      sum
+    end
+  end
+end
---
+++ 2017/day1/part_2.rb
@@ -0,0 +1,14 @@
+module Day1
+  class Part2
+    def self.run(s_input)
+      return 0 if s_input.length < 2
+      jump = s_input.length / 2
+      sum = 0
+      s_input.chars.each_with_index do |curr_digit, index|
+        pair = s_input[(index + jump) % s_input.length]
+        sum += curr_digit.to_i if pair == curr_digit
+      end
+      sum
+    end
+  end
+end
