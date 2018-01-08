1721922e5edcb95c993bac559a23111ff8620791
Dunya Kirkali <dunya.kirkali@icemobile.com>
2018-01-09 00:18:09 +0100

    2015 Day 10 Part 2

---
+++ 2015/day10/part_2.exs
@@ -0,0 +1,18 @@
+defmodule M do
+  def look_n_say(str, acc) when acc == 50, do: str
+  def look_n_say(str, acc) when acc < 50 do
+    new_str = str
+    |> String.graphemes
+    |> Enum.chunk_by(fn arg -> arg end)
+    |> Enum.map(fn(chunk) ->
+      Integer.to_string(length(chunk)) <> List.first(chunk)
+    end)
+    |> Enum.join
+    look_n_say(new_str, acc + 1)
+  end
+end
+
+"1113122113"
+|> M.look_n_say(0)
+|> String.length
+|> IO.inspect
