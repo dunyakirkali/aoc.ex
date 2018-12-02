24bdb9d3df93d2366984ef33d3cf39305d8e408b
Dunya Kirkali <dunyakirkali@gmail.com>
2018-12-02 06:34:28 +0100

    2018 Day 1 Part 1

--- 2018/day2/part_1.exs
+++ 2018/day2/part_1.exs
@@ -1,5 +1,287 @@
 input = """
+mphcuiszrnjzxwkbgdzqeoyxfa
+mihcuisgrnjzxwkbgdtqeoylia
+mphauisvrnjgxwkbgdtqeiylfa
+mphcuisnrnjzxwkbgdgqeoylua
+mphcuisurnjzxwkbgdtqeoilfi
+mkhcuisvrnjzowkbgdteeoylfa
+mphcoicvrnjzxwksgdtqeoylfa
+mxhcuisvrndzxwkbgdtqeeylfa
+dphcuisijnjzxwkbgdtqeoylfa
+mihvuisvrqjzxwkbgdtqeoylfa
+mphcuisrrnvzxwkbgdtqeodlfa
+mphtuisdrnjzxskbgdtqeoylfa
+mphcutmvsnjzxwkbgdtqeoylfa
+mphcunsvrnjzswkggdtqeoylfa
+mphcuisvrwjzxwkbpdtqeoylfr
+mphcujsdrnjzxwkbgdtqeovlfa
+mpfcuisvrdjzxwkbgdtteoylfa
+mppcuisvrpjzxwkbgdtqeoywfa
+mphcuisvrnjzxwkbfptqroylfa
+mphcuisvrnjzxwkbgstoeoysfa
+mphcufsvrnjzcwkbgdeqeoylfa
+mphcuissrnjzxwkbgdkquoylfa
+sphcuxsvrnjzxwkbgdtqioylfa
+mphcuiivrhjzxwkbgdtqevylfa
+echcuisvrnjzxwkbgltqeoylfa
+mphcuisvrljexwkbvdtqeoylfa
+mpjcuisvrnjzxwkhidtqeoylfa
+mphcuisvrfjzmwkbgdtqeoylfl
+mwhcuisvrnjzxwkbgdtqeoytfm
+mphcuisvrsjzxwkbgdaqeoylfh
+mohcuisvrnjzxwkbgdtqtoymfa
+maycuisvrnjzxwkbgdtqboylfa
+pphcuisvqnjzxwkbgdtqeoylfd
+mprcuisvrnjtxwmbgdtqeoylfa
+mfhcuisgrnjzxckbgdtqeoylfa
+mphiubsvrnjzxwkbgdtqeoyufa
+dphctisvrnjzxwkbgdtqeoylfk
+mphcuisvrnjznwksgdtqeoyzfa
+mpwcuisvrnjziwkbgdtqaoylfa
+mphduzsvrnjznwkbgdtqeoylfa
+mphccisvrnjzxwebgdtqeoylqa
+xphcuisvrnjzxwkfvdtqeoylfa
+mphcupsvrnjzxwkbgdtfeoylpa
+mphcuisvrtjzjwkbgdtqeoylfe
+mpbcuisvrnjzxwkbgdmieoylfa
+mphcuisvrnjzxwkbgjtqetylaa
+mphcuisvrnjzxwpbgdtgdoylfa
+ophcufsvrqjzxwkbgdtqeoylfa
+iphcuhsvrnjzxwkbgetqeoylfa
+mphcuisvunjzxwwbgdtqeoylqa
+mphcpisvrnjzowkbgdtveoylfa
+mphcuisvrnjzxhkbgdtqeotlla
+mphcuisvrnjzxwkbodtgeoylha
+mphcuisvrjjzxwkbwdtqtoylfa
+mphcwisvrnjnxwkbgjtqeoylfa
+mplcuicqrnjzxwkbgdtqeoylfa
+mphcuisvrnjzxydbgdtqeoylfn
+ophckisvrnjzxwkbgdtqeozlfa
+mphcuisvrkjzxwkbgdtteoblfa
+yphcuisvrnjcxwkbggtqeoylfa
+mphcuisvrnazxwfbqdtqeoylfa
+mphcuisvrmjzxwkbgdtlwoylfa
+mphctksvrnjzxwibgdtqeoylfa
+mphcuisprnjzxlebgdtqeoylfa
+mphcuisnrnjzxakbgdtueoylfa
+mphcuiavrnjoxwtbgdtqeoylfa
+nphcuisvrnjzxwkbgdtqzoylfk
+mphcuisrrnjmxwkbgdtqdoylfa
+mphcuisvrujzxwkvgdtqehylfa
+mphcuisvrnfzxwkogdtqebylfa
+mphcuisvrnjwdwkbgdtqeoyxfa
+mphcuisvrntzxwkrgxtqeoylfa
+mpzcuisvrnjzxwebgdtqeoylsa
+aphcuikvrnjzxwwbgdtqeoylfa
+mphcqisvrnjzxwkpgdtqeoelfa
+mphcuusvrnjzxwkbgdtjeodlfa
+mphcuisvrnjzewkbgdtteoylza
+mphcuisvanjzxwkbgdtheoylfc
+mphcjishrnjzxwkbgltqeoylfa
+mpxcuislrnjzxwkbgdtqeoynfa
+mphcuisvrnjjxwkbgdtmeoxlfa
+mphcimsvrnjzxwkbsdtqeoylfa
+mphcxisvcnjzxwjbgdtqeoylfa
+mphcuisbrvjzxwkbgdtqeoymfa
+mplcuisvrnjzxwkbgdtaenylfa
+mphcuihvrnjzxwkygytqeoylfa
+mphcbisvrnjzxhkbgdtqezylfa
+mphcuisarnjzxwkbgatqeoylfv
+mphcumsvrnjzxwkbgdrqebylfa
+mlhcuisvrnwzxwkbgdtqeoylfx
+mpkcuisvrkjzxwkbgdtqeoylfo
+mphcuissrnjzxwkbgdtqmoylfc
+mphcuiwvrnjuxwkfgdtqeoylfa
+mphcuicvlnjzxwkbgdvqeoylfa
+mphcuisvrvvzxwkbfdtqeoylfa
+myhcuisvrnjpxwkbgntqeoylfa
+mpocuisvrnjzxwtbgitqeoylfa
+mphcuisvrnjzxwkbgdtwewyqfa
+mphcuisvtnjzxwwbgdtqeoolfa
+mphcuisvrnjzxgkbgdyqeoyyfa
+mphcuisvrdjzxwkbgpyqeoylfa
+bphcuisvrnjzxwkbgxtqefylfa
+sphcuisvrdjzxwktgdtqeoylfa
+mphcuvsvrnjmxwobgdtqeoylfa
+mphcuisvrnjzxwkbsdtqeuylfb
+mnhcmisvynjzxwkbgdtqeoylfa
+mphckisvrnjzxwkhgdkqeoylfa
+mpacuisvrnjzxwkbgdtqeoolaa
+mpgcuisvrnjzxwkbzdtqeoynfa
+mphcuisvrojzxwkbzdtqeoylga
+mphcuisvknjfxwkbydtqeoylfa
+mphcuistrnjzxwkbgdqqeuylfa
+bpvcuiszrnjzxwkbgdtqeoylfa
+mphcuxsvrnjzswkbgdtqeoelfa
+mphcuisvbnjzxwlbgdtqeoylla
+mphcuisvonczxwkbgktqeoylfa
+mphcuisvrnkzxwvbgdtquoylfa
+mphcuisvrnjzxokfgdtqeoylia
+tphcuisvrnjzxwkbjdwqeoylfa
+mihcuisvrnjzpwibgdtqeoylfa
+mphcuisvrejzxwkbgdtqjuylfa
+mprcuisvrnjixwkxgdtqeoylfa
+mpqcuiszrnjzxwkbgdtqeodlfa
+mphcuasvrnjzzakbgdtqeoylva
+mphcuisvrnjzmwkbtdtqeoycfa
+mphcuisvrnjzxwkbcdtqioylxa
+mphckisvrnjzxwkbcdtqeoylfm
+mphcuisvrnjuxwbogdtqeoylfa
+mphcuisdrnjzxwkbldtqeoylfx
+mphcuisvrnjoxwkbgdtqeyyyfa
+mphcuicvqnjzxwkbgdtqeoylna
+mpmcuisvrnjzxwkbgdtqktylfa
+mphcuisvrnqzxwkggdtqeoykfa
+mphcuisvryjzxwkbydtqejylfa
+mphcugsvrnjzxwkbghtqeeylfa
+rphcuusvrnjzxwkwgdtqeoylfa
+zphwuiyvrnjzxwkbgdtqeoylfa
+cphcuivvrnjzxwkbgdtqenylfa
+mphcuisvrnjzxwkagotqevylfa
+mprcuisvrcjzxwkbgdtqeoytfa
+mphjugsvrnezxwkbgdtqeoylfa
+mphcuisvryjzxwkbgltqeoylaa
+mphcursvrnjzxfkbgdtqeoydfa
+mphcuisvrcuzxwkbgdtqeoylfw
+mphcuisvrijzxwkbgdtqeoelfh
+xphcuisvenjzxjkbgdtqeoylfa
+mphcuisvrnazxwkbgdeqeoylaa
+mphcuisbrsjzxwkbgdtqeoygfa
+mlhvuisvrnjzxwkbgdtqeoylfh
+mphcuisvrnjzxukbgdtqeoyhfy
+mpzcuilvrnjzawkbgdtqeoylfa
+hphcuisjfnjzxwkbgdtqeoylfa
+mahcuisvrnjzxwkegdtqeoylfi
+mphcuixvrnjzcwkbgdtqetylfa
+mphcuisvrnjzxwkdgdtqeoklfj
+mlhcuisvrnjzxwkbgdteeoylka
+mphcuifvrnjbxwkrgdtqeoylfa
+mphcuasvrnjzzwkbgdtqeoylva
+mphcuisvrnjzxwkboutqeoylba
+mbhcuisvcnjzxwklgdtqeoylfa
+mpbcuisvrnjzxgkbgdtqesylfa
+mphcuisvrnjfswkbgdtqeoylfd
+mphcuisvrnjzxwkbgdoweoysfa
+uphcuisvrnjzrwkbgdtqelylfa
+mphcuisvrnjzxwkbgdtqyoylsi
+mpqcuiqvxnjzxwkbgdtqeoylfa
+mphcuisorfjzxwkbgatqeoylfa
+mphcuisvrntfxwkbzdtqeoylfa
+mphcuisvrnrzxwkbgdtueoylfl
+mphcuisvrnjzewkagdtyeoylfa
+mpocuisdrnjzxwkbgdtqeozlfa
+mphcuisvrnjjxwkbgdtoeoylfm
+mphcuisvenjzxwkbgdtqwoylza
+mpmcuisvrnjzxwkbgdtqeoxlfr
+mphcuisvgnjhxwkbgdtqeoplfa
+mphcuisvrnjzowkdgdtqeoyyfa
+mphcuisqynjzxwkbgdtqeoylda
+hphcuisvgnjzxwkbgdtbeoylfa
+iphcuipvrnuzxwkbgdtqeoylfa
+mphcuisvrnjzsikbpdtqeoylfa
+mpwcuhsvrnjzxbkbgdtqeoylfa
+mnhjuisvcnjzxwkbgdtqeoylfa
+mphcudsvrnjzxwkbgdtqloilfa
+mpncuiwvrwjzxwkbgdtqeoylfa
+mphcuisvrnjgawkbgdtqeoylya
+mphcuisvrnjzxwkbggtteoslfa
+mphcuisvrnjzxwkbgdvqeoylpe
+mphcuisvrnczxfkbgktqeoylfa
+mphcuifvrnjzxwkbgdbmeoylfa
+mphcuisvrnjytwkbgdtqeoylla
+mphcuisvrnjzxwkbgdtjeoxlfn
+mphjuisvrnjzxwkbghtqeoyffa
+mphcuisvrnjzxkrbgdtqeoylaa
+mphcbisvrnjzxwkbgttqeoylfs
+mphkuksvbnjzxwkbgdtqeoylfa
+nphcuidvrnjzxwhbgdtqeoylfa
+mphguzsvrnjzxwkbgdaqeoylfa
+mihcuisfrnjzxwkbgdtqhoylfa
+mphcuisvrnrzxwpbgdtqesylfa
+zphcuisvrnjzxwkbddtqeoylaa
+mphcuigvmnjzxwkbgdtqeoylba
+mjhcuisvrnjzxjkbgdtqeoylha
+mphnuisvrnjznwkbgdtqnoylfa
+mkhcuisvrnjcxwkbgdqqeoylfa
+mphcuisvenjzxwbbqdtqeoylfa
+qphcuisnrnjzawkbgdtqeoylfa
+mphcuisvrdjzxwkbgdtqeoywca
+mphcuzsvvnjzxwfbgdtqeoylfa
+pphcuxsvrnjzxwkbgdtmeoylfa
+mphiuvsvrnjzxlkbgdtqeoylfa
+mphlqisvrnjzxkkbgdtqeoylfa
+mmhcuisvrnjzxwkbgatqeoylea
+mphduisrrnjoxwkbgdtqeoylfa
+mphcuisvrnjnxwkvgdyqeoylfa
+mphcuvsvrnjzxgkbgdtqeoylfz
+mphcuisvryjzxwkbggtqkoylfa
+iphcuisvrdjzxwkbgotqeoylfa
+mphcuisvrnjzxwhbgdtqwoyofa
+mphcorbvrnjzxwkbgdtqeoylfa
+mghcuisvrnpzxykbgdtqeoylfa
+mphauisvrnjnxwkbzdtqeoylfa
+mphcgisvrnjzxwkwgdtqeoygfa
+mphcuisvrnjzxwkggotqeoylba
+mphcuesvrnjzxwkbgdwqebylfa
+yphcuisvrnjzxwkbgdxqeoylja
+ephyuisvrnjzywkbgdtqeoylfa
+mfhcuisqrnjzxwkbgdlqeoylfa
+mphkuisvrnjzxwkbertqeoylfa
+mphcuusgrnjzxwkbggtqeoylfa
+mphcuildrnjvxwkbgdtqeoylfa
+mphcuiuvrnjzlwkbgwtqeoylfa
+mppcuisvrljzxwkbgdtqeoylfw
+mphcwiwvrnjzxwsbgdtqeoylfa
+mphcubivrnjzxwkqgdtqeoylfa
+mphcuisvrnjpxwkngdtqeoylpa
+pchcuisvrgjzxwkbgdtqeoylfa
+mphcuisvlnjzxwkbgdtmeoylfw
+mphcuisvrnjzywkbgdvqeoylfj
+mpzcuisvrnezxwktgdtqeoylfa
+mphcuisvrnjbxwkbgzrqeoylfa
+mphcuisvrnjzxwktgdtqeodtfa
+jphcuiavrnjzxwkbgdtqeoylfv
+mphcuisvrnjzxwkbddppeoylfa
+mphcuissrkjzxwkbgxtqeoylfa
+mphcuisvrhjzxwxbgdtqeoylxa
+mphcvisvgnjjxwkbgdtqeoylfa
+mphcuisprnjwxwtbgdtqeoylfa
+mphcuissrnjzxqkbgdtqeoymfa
+mphcuiabrnjzxokbgdtqeoylfa
+mphcuisvrnczxwkbgmtpeoylfa
 """
 
-input
+counts = input
+  |> String.trim
+  |> String.split
+  |> Enum.map(&String.graphemes/1)
+  |> Enum.map(fn(list) ->
+    Enum.reduce(list, %{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+      |> Enum.filter(fn {_, val} ->
+        val == 2 || val == 3
+      end)
+  end)
+  |> Enum.reduce({0, 0}, fn x, acc ->
+    list = x
+      |> Map.new
+      |> Map.values
+      |> Enum.uniq
+
+    if Enum.member?(list, 2) do
+      new_acc = put_elem(acc, 0, elem(acc, 0) + 1)
+      if Enum.member?(list, 3) do
+        put_elem(new_acc, 1, elem(new_acc, 1) + 1)
+      else
+        new_acc
+      end
+    else
+      if Enum.member?(list, 3) do
+        put_elem(acc, 1, elem(acc, 1) + 1)
+      else
+        acc
+      end
+    end
+  end)
+
+elem(counts, 0) * elem(counts, 1)
   |> IO.puts
