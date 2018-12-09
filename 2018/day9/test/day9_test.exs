0241bcee9c282c9df722d3b51fb235d367119ac4
Dunya Kirkali <dunyakirkali@gmail.com>
2018-12-09 14:15:06 +0100

    Correct 2018 Day 9 test

--- 2018/day9/test/day9_test.exs
+++ 2018/day9/test/day9_test.exs
@@ -3,6 +3,10 @@ defmodule Day9Test do
   doctest Day9
 
   test "highest score" do
-    assert Day9.highest_score(427, 70723) == 0
+    assert Day9.highest_score(427, 70_723) == 399745
+  end
+
+  test "highest score 100x" do
+    assert Day9.highest_score(427, 7_072_300) == 0
   end
 end
