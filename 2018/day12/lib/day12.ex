4a8d3cd69f1e4bf63be6c120adfd91f7307a3a63
Dunya Kirkali <dunyakirkali@gmail.com>
2018-12-13 00:30:15 +0100

    2018 Day 12 Part 2

--- 2018/day12/lib/day12.ex
+++ 2018/day12/lib/day12.ex
@@ -1,32 +1,159 @@
 defmodule Day12 do
+  use Memoize
+
   @moduledoc """
   Documentation for Day12.
   """
 
-  def iterate(_, string, 0), do: string
-  def iterate(filename, string, generations) do
-    IO.inspect(generations, label: "Gen")
-    codepoints =
-      string
-      # |> IO.inspect
-      |> String.codepoints
+  @pad_size 5
+
+  def part_2(filename, string) do
+    lo = Day12.iterate(filename, string, 2000, 0)
+    |> sum
+
+    high = Day12.iterate(filename, string, 2001, 0)
+    |> sum
+
+    (50_000_000_000 - 2001) * (high - lo) + high
+  end
+
+  def pmap(collection, func) do
+    collection
+    |> Enum.map(&(Task.async(fn -> func.(&1) end)))
+    |> Enum.map(&Task.await/1)
+  end
+
+  def sum(res) do
+    res
+    |> elem(0)
+    |> String.split("")
+    |> Enum.with_index(-1 * elem(res, 1) - 1)
+    |> Enum.reduce(0, fn {char, val}, acc ->
+      if char == "#", do: acc + val, else: acc
+    end)
+  end
+
+  def iterate(_, string, 0, index), do: normalize({string, index})
+  def iterate(filename, string, generations, index) do
+    {string, index} = normalize({string, index})
+
+    codepoints = String.codepoints(string)
 
     chunks =
       codepoints
-      |> Enum.chunk_every(5, 1, :discard)
+      |> Enum.chunk_every(@pad_size, 1, :discard)
 
-    # IO.inspect(length(chunks), label: "chunks")
     lookedup =
       chunks
-      |> Enum.map(fn list -> Enum.join(list, "") end)
-      |> Enum.map(fn chunk -> lookup(filename, chunk) end)
-
-    # IO.inspect(length(lookedup), label: "next")
+      |> pmap(fn list -> Enum.join(list, "") end)
+      |> pmap(fn chunk -> lookup(filename, chunk) end)
 
     next_iteration = Enum.take(codepoints, 2) ++ lookedup ++ Enum.take(codepoints, -2)
 
-    # IO.inspect(length(next_iteration), label: "next")
-    iterate(filename, Enum.join(next_iteration, ""), generations - 1)
+    iterate(filename, Enum.join(next_iteration, ""), generations - 1, index)
+  end
+
+  @doc """
+      iex> Day12.normalize({"#", 0})
+      {".....#.....", 5}
+
+      iex> Day12.normalize({"#....", 1})
+      {".....#.....", 6}
+
+      iex> Day12.normalize({"....#", -1})
+      {".....#.....", 0}
+
+      iex> Day12.normalize({".....#...#....#.....#..#..#..#.....", 5})
+      {".....#...#....#.....#..#..#..#.....", 5}
+  """
+  defmemo normalize(string_and_index) do
+    string_and_index
+    |> expand_leading
+    |> expand_trailing
+    |> shrink_leading
+    |> shrink_trailing
+  end
+
+  @doc """
+      iex> Day12.expand_trailing({"#...", 0})
+      {"#.....", 0}
+
+      iex> Day12.expand_trailing({"..#...", 3})
+      {"..#.....", 3}
+  """
+  defmemo expand_trailing({string, index}) do
+    rev_string = String.reverse(string)
+    {shrunk, _} = expand_leading({rev_string, index})
+    {String.reverse(shrunk), index}
+  end
+
+  @doc """
+      iex> Day12.expand_leading({"#", 0})
+      {".....#", 5}
+
+      iex> Day12.expand_leading({"#..", 0})
+      {".....#..", 5}
+
+      iex> Day12.expand_leading({"...#..", 0})
+      {".....#..", 2}
+
+      iex> Day12.expand_leading({"....#.#...#..#.#....#..#..#...#.....", 0})
+      {".....#.#...#..#.#....#..#..#...#.....", 1}
+  """
+  defmemo expand_leading({string, index}) do
+    leading_dot_count =
+      string
+      |> String.codepoints()
+      |> Enum.with_index()
+      |> Enum.take_while(fn {char, index} ->
+        char != "#" && index != @pad_size
+      end)
+      |> pmap(fn {char, _} -> char end)
+      |> Enum.filter(fn x -> x == "." end)
+      |> Enum.count()
+
+    if leading_dot_count == 5 do
+      {string, index}
+    else
+      diff = @pad_size - leading_dot_count
+      {String.pad_leading(string, String.length(string) + diff, "."), index + diff}
+    end
+  end
+
+  @doc """
+      iex> Day12.shrink_trailing({"......#..........", 0})
+      {"......#.....", 0}
+
+      iex> Day12.shrink_trailing({"#.......", 0})
+      {"#.....", 0}
+  """
+  defmemo shrink_trailing({string, index}) do
+    rev_string = String.reverse(string)
+    {shrunk, _} = shrink_leading({rev_string, index})
+    {String.reverse(shrunk), index}
+  end
+
+  @doc """
+      iex> Day12.shrink_leading({"........#", 0})
+      {".....#", -3}
+
+      iex> Day12.shrink_leading({"........#..", 3})
+      {".....#..", 0}
+  """
+  defmemo shrink_leading({string, index}) do
+    leading_dot_count =
+      string
+      |> String.codepoints()
+      |> Enum.take_while(fn char -> char != "#" end)
+      |> Enum.filter(fn x -> x == "." end)
+      |> Enum.count()
+
+    if leading_dot_count == 5 do
+      {string, index}
+    else
+      diff = leading_dot_count - @pad_size
+      {String.slice(string, diff..-1), index - diff}
+    end
   end
 
   @doc """
@@ -42,8 +169,9 @@ defmodule Day12 do
       iex> Day12.lookup("priv/example.txt", "####.")
       "#"
   """
-  def lookup(filename, rule) do
+  defmemo lookup(filename, rule) do
     lookup = rules(filename)[rule]
+
     if lookup == nil do
       "."
     else
@@ -51,18 +179,25 @@ defmodule Day12 do
     end
   end
 
-  def rules(filename) do
+  defmemo rules(filename) do
     filename
     |> File.read!()
     |> String.split("\n", trim: true)
     |> Enum.with_index()
     |> Enum.filter(fn {_, index} -> index > 0 end)
-    |> Enum.map(fn {line, _} -> String.split(line, " => ") end)
+    |> pmap(fn {line, _} -> String.split(line, " => ") end)
     |> Enum.reduce(%{}, fn [from, to], acc ->
       Map.put(acc, from, to)
     end)
   end
 
+  @doc """
+      iex> Day12.initial_state("priv/example.txt")
+      "#..#.#..##......###...###"
+
+      iex> Day12.initial_state("priv/input.txt")
+      "..##.#######...##.###...#..#.#.#..#.##.#.##....####..........#..#.######..####.#.#..###.##..##..#..#"
+  """
   def initial_state(filename) do
     filename
     |> File.read!()
@@ -70,6 +205,6 @@ defmodule Day12 do
     |> Enum.with_index()
     |> Enum.filter(fn {_, index} -> index == 0 end)
     |> Enum.map(fn {line, _} -> List.last(String.split(line, ": ")) end)
-    |> List.first
+    |> List.first()
   end
 end
--- 2018/day12/test/day12_test.exs
+++ 2018/day12/test/day12_test.exs
@@ -2,83 +2,51 @@ defmodule Day12Test do
   use ExUnit.Case
   doctest Day12
 
-  test "initial_state for example" do
-    assert Day12.initial_state("priv/example.txt") == "#..#.#..##......###...###"
-  end
-
-  test "initial_state for input" do
-    assert Day12.initial_state("priv/input.txt") == "..##.#######...##.###...#..#.#.#..#.##.#.##....####..........#..#.######..####.#.#..###.##..##..#..#"
-  end
-
   test "iterate 0 step for example" do
     filename = "priv/example.txt"
     string = Day12.initial_state(filename)
-    string = String.pad_leading(string, String.length(string) + 20, ".")
-    string = String.pad_trailing(string, String.length(string) + 20, ".")
-    assert Day12.iterate(filename, string, 0) == "....................#..#.#..##......###...###...................."
+    assert Day12.iterate(filename, string, 0, 0) == {".....#..#.#..##......###...###.....", 5}
   end
 
   test "iterate 1 step for example" do
     filename = "priv/example.txt"
     string = Day12.initial_state(filename)
-    string = String.pad_leading(string, String.length(string) + 20, ".")
-    string = String.pad_trailing(string, String.length(string) + 20, ".")
-    assert Day12.iterate(filename, string, 1) == "....................#...#....#.....#..#..#..#...................."
+    assert Day12.iterate(filename, string, 1, 0) == {".....#...#....#.....#..#..#..#.....", 5}
+  end
+
+  test "iterate 2 step for example" do
+    filename = "priv/example.txt"
+    string = Day12.initial_state(filename)
+    assert Day12.iterate(filename, string, 2, 0) == {".....##..##...##....#..#..#..##.....", 5}
+  end
+
+  test "iterate 3 step for example" do
+    filename = "priv/example.txt"
+    string = Day12.initial_state(filename)
+    assert Day12.iterate(filename, string, 3, 0) == {".....#.#...#..#.#....#..#..#...#.....", 6}
   end
 
   test "iterate 20 step for example" do
     filename = "priv/example.txt"
     string = Day12.initial_state(filename)
-    string = String.pad_leading(string, String.length(string) + 20, ".")
-    string = String.pad_trailing(string, String.length(string) + 20, ".")
-    res = Day12.iterate(filename, string, 20)
-    assert res == "..................#....##....#####...#######....#.#..##.........."
+    res = Day12.iterate(filename, string, 20, 0)
 
-    sum =
-      res
-      |> String.split("")
-      |> Enum.with_index(-21)
-      |> Enum.reduce(0, fn {char, val}, acc ->
-        if char == "#", do: acc + val, else: acc
-      end)
-    assert sum == 325
+    assert res == {".....#....##....#####...#######....#.#..##.....", 7}
+    assert Day12.sum(res) == 325
   end
 
   test "iterate 20 step for input" do
     filename = "priv/input.txt"
-    pad = 100
     string = Day12.initial_state(filename)
-    string = String.pad_leading(string, String.length(string) + pad, ".")
-    string = String.pad_trailing(string, String.length(string) + pad, ".")
-    res = Day12.iterate(filename, string, 20)
-    # assert res == ".................#.#..##..##..##..####..#.....#.###.#......##..#....##..#......##..##..###.#......#.#..#.#.......##..#....##..##..#....#...."
+    res = Day12.iterate(filename, string, 20, 0)
 
-    sum =
-      res
-      |> String.split("")
-      |> Enum.with_index((pad + 1) * -1)
-      |> Enum.reduce(0, fn {char, val}, acc ->
-        if char == "#", do: acc + val, else: acc
-      end)
-    assert sum == 2542
+    assert Day12.sum(res) == 2542
   end
 
   test "iterate 50000000000 step for input" do
     filename = "priv/input.txt"
-    pad = 100_000
     string = Day12.initial_state(filename)
-    string = String.pad_leading(string, String.length(string) + pad, ".")
-    string = String.pad_trailing(string, String.length(string) + pad, ".")
-    res = Day12.iterate(filename, string, 50_000_000_000)
-    # assert res == ".................#.#..##..##..##..####..#.....#.###.#......##..#....##..#......##..##..###.#......#.#..#.#.......##..#....##..##..#....#...."
 
-    sum =
-      res
-      |> String.split("")
-      |> Enum.with_index((pad + 1) * -1)
-      |> Enum.reduce(0, fn {char, val}, acc ->
-        if char == "#", do: acc + val, else: acc
-      end)
-    assert sum == 2542
+    assert Day12.part_2(filename, string) == 2_550_000_000_883
   end
 end
