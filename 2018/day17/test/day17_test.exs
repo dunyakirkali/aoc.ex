4527d0c91246572d859e586d6855cec533452a7b
Dunya Kirkali <dunyakirkali@gmail.com>
2018-12-18 08:55:10 +0100

    2018 Day17

--- 2018/day17/test/day17_test.exs
+++ 2018/day17/test/day17_test.exs
@@ -5,16 +5,16 @@ defmodule Day17Test do
   test "Part 1 example" do
     assert Day17.part_1("priv/example.txt") == 57
   end
-  
-  test "Part 1" do
-    assert Day17.part_1("priv/input.txt") == 26910
-  end
-  
-  test "Part 2 example" do
-    assert Day17.part_2("priv/example.txt") == 29
-  end
-  
-  test "Part 2" do
-    assert Day17.part_2("priv/input.txt") == 22182
-  end
+  # 
+  # test "Part 1" do
+  #   assert Day17.part_1("priv/input.txt") == 26910
+  # end
+  # 
+  # test "Part 2 example" do
+  #   assert Day17.part_2("priv/example.txt") == 29
+  # end
+  # 
+  # test "Part 2" do
+  #   assert Day17.part_2("priv/input.txt") == 22182
+  # end
 end
