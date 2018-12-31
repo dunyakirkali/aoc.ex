8b8594963d863e6609ab1f71b986a7b4548d263b
Dunya Kirkali <dunyakirkali@gmail.com>
2018-12-31 08:39:56 -0500

    2018 Day 24

---
+++ 2018/day24/test/day24_test.exs
@@ -0,0 +1,12 @@
+defmodule Day24Test do
+  use ExUnit.Case
+  doctest Day24
+  
+  test "Part 1" do
+    assert Day24.part_1() == 9878
+  end
+  
+  test "Part 2" do
+    assert Day24.part_2() == 10954
+  end
+end
