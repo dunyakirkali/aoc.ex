34c1ad67b230e0ef5552b4f6a1c36e274a7b27a8
Dunya Kirkali <dunyakirkali@gmail.com>
2019-01-07 10:10:23 +0100

    2016 Day 2

--- 2016/day2/lib/day2.ex
+++ 2016/day2/lib/day2.ex
@@ -4,13 +4,38 @@ defmodule Day2 do
   """
 
   @doc """
+      iex> Day2.part_2([
+      ...>   "ULL",
+      ...>   "RRDDD",
+      ...>   "LURDL",
+      ...>   "UUUUD"
+      ...> ])
+      "5DB3"
+  """  
+  def part_2(lines) do
+    lines
+    |> Enum.reduce({[], {0, 2}}, fn line, {acc, pos} ->
+      pos = 
+        line
+        |> String.split("", trim: true)
+        |> Enum.reduce(pos, fn item, pos ->
+          move_2(pos, item)
+        end)
+      {[key(pos, 1) | acc], pos}
+    end)
+    |> elem(0)
+    |> Enum.reverse()
+    |> Enum.join()
+  end
+
+  @doc """
       iex> Day2.part_1([
       ...>   "ULL",
       ...>   "RRDDD",
       ...>   "LURDL",
       ...>   "UUUUD"
       ...> ])
-      1985
+      "1985"
   """
   def part_1(lines) do
     lines
@@ -21,22 +46,21 @@ defmodule Day2 do
         |> Enum.reduce(pos, fn item, pos ->
           move(pos, item)
         end)
-      {[key(pos) | acc], pos}
+      {[key(pos, 0) | acc], pos}
     end)
     |> elem(0)
     |> Enum.reverse()
     |> Enum.join()
-    |> String.to_integer()
   end
   
   @doc """
-      iex> Day2.key({1, 1})
+      iex> Day2.key({1, 1}, 0)
       5
       
-      iex> Day2.key({2, 2})
+      iex> Day2.key({2, 2}, 0)
       9
   """
-  def key(pos) do
+  def key(pos, type) when type == 0 do
     case pos do
       {0, 0} -> 1
       {1, 0} -> 2
@@ -47,6 +71,35 @@ defmodule Day2 do
       {0, 2} -> 7
       {1, 2} -> 8
       {2, 2} -> 9
+    end
+  end
+  
+  @doc """
+      iex> Day2.key({1, 1}, 1)
+      "2"
+      
+      iex> Day2.key({2, 2}, 1)
+      "7"
+      
+      iex> Day2.key({0, 0}, 1)
+      nil
+  """
+  def key(pos, type) when type == 1 do
+    case pos do
+      {2, 0} -> "1"
+      {1, 1} -> "2"
+      {2, 1} -> "3"
+      {3, 1} -> "4"
+      {0, 2} -> "5"
+      {1, 2} -> "6"
+      {2, 2} -> "7"
+      {3, 2} -> "8"
+      {4, 2} -> "9"
+      {1, 3} -> "A"
+      {2, 3} -> "B"
+      {3, 3} -> "C"
+      {2, 4} -> "D"
+      _ -> nil
     end
   end
   
@@ -73,6 +126,24 @@ defmodule Day2 do
         {max(0, x - 1), y}
       "R" ->
         {min(2, x + 1), y}
+    end
+  end
+  
+  
+  def move_2({x, y}, direction) do
+    case direction do
+      "U" ->
+        newY = max(0, y - 1)
+        if key({x, newY}, 1) == nil, do: {x, y}, else: {x, newY}
+      "D" ->
+        newY = min(4, y + 1)
+        if key({x, newY}, 1) == nil, do: {x, y}, else: {x, newY}
+      "L" ->
+        newX = max(0, x - 1)
+        if key({newX, y}, 1) == nil, do: {x, y}, else: {newX, y}
+      "R" ->
+        newX = min(4, x + 1)
+        if key({newX, y}, 1) == nil, do: {x, y}, else: {newX, y}
     end
   end
 end
--- 2016/day2/test/day2_test.exs
+++ 2016/day2/test/day2_test.exs
@@ -8,15 +8,15 @@ defmodule Day2Test do
       |> File.read!()
       |> String.split("\n", trim: true)
   
-    assert Day2.part_1(input) == 78985
+    assert Day2.part_1(input) == "78985"
   end
 
-  # test "Part 2" do
-  #   input =
-  #     'priv/input.txt'
-  #     |> File.read!()
-  #     |> String.split(", ", trim: true)
-  # 
-  #   assert Day1.part_2(input) == 136
-  # end
+  test "Part 2" do
+    input =
+      'priv/input.txt'
+      |> File.read!()
+      |> String.split("\n", trim: true)
+  
+    assert Day2.part_2(input) == "57DD8"
+  end
 end
