6edf4fdd2b09590b8cf2ad67d5a9e1fb379d3eb9
Dunya Kirkali <dunyakirkali@gmail.com>
2019-10-23 07:22:39 +0200

    Day 19 Part 1

--- 2016/day19/lib/day19.ex
+++ 2016/day19/lib/day19.ex
@@ -1,4 +1,8 @@
 defmodule Day19 do
+  defmodule Elf do
+    defstruct [:id, :left]
+  end
+
   @doc """
       iex> Day19.part_1(5)
       3
@@ -6,44 +10,42 @@ defmodule Day19 do
   def part_1(elf_count) do
     elves =
       0..(elf_count - 1)
-      |> Enum.map(fn elf ->
-        {elf, 1}
+      |> Enum.map(fn elf_id ->
+        %Elf{id: elf_id + 1, left: rem(elf_id + 1, elf_count) }
       end)
-      |> Enum.into(%{})
-      |> IO.inspect()
-  
-    turn(elves, 0)
+      |> turn(0)
   end
-  
+
+  @doc """
+      iex> Day19.part_1_shortcurt(5)
+      3
+  """
+  def part_1_shortcurt(elf_count) do
+    binary_array =
+      elf_count
+      |> Integer.to_string(2)
+      |> String.codepoints
+
+    {first, rest} = Enum.split(binary_array, 1)
+
+    rest ++ first
+    |> Enum.join
+    |> Integer.parse(2)
+    |> elem(0)
+  end
+
   def turn(elves, index) do
-    elves_with_presents =
-      elves
-      |> Enum.filter(fn {elf, present} ->
-        present == 1
-      end)
-  
-    if Enum.count(elves_with_presents) == 1 do
-      index
+    # IO.puts("--")
+    elf = Enum.at(elves, index)# |> IO.inspect(label: "elf")
+    next_elf = Enum.at(elves, elf.left)# |> IO.inspect(label: "next")
+
+    if elf.id == next_elf.id do
+      elf.id
     else
-      if Map.get(elves, index) == 0 do
-        IO.puts("Elf #{index} has no presents and is skipped.")
-        turn(elves, index + 1)
-      else
-        next_key =
-          elves
-          |> Enum.to_list
-          |> Enum.sort
-          |> List.duplicate(2)
-          |> List.flatten
-          # |> IO.inspect(label: "a")
-          |> Enum.drop(index + 1)
-          # |> IO.inspect(label: "b")
-          |> Enum.find(fn {k, v} -> v == 1 end)
-          # |> IO.inspect(label: "c")
-          |> elem(0)
-        IO.puts("Elf #{index} takes Elf #{next_key}'s present.")
-        turn(Map.put(elves, next_key, 0), rem(index + 1, map_size(elves)))
-      end
+      elf = %{elf | left: next_elf.left}
+      elves = List.replace_at(elves, index, elf)# |> IO.inspect(label: "elves")
+
+      turn(elves, elf.left)
     end
   end
 end
--- 2016/day19/test/day19_test.exs
+++ 2016/day19/test/day19_test.exs
@@ -2,7 +2,11 @@ defmodule Day19Test do
   use ExUnit.Case
   doctest Day19
 
-  # test "Part 1" do
-  #   assert Day19.part_1(3014603) == 0
-  # end
+  test "Part 1" do
+    assert Day19.part_1(3014603) == 1834903
+  end
+
+  test "Part 1 shortcut" do
+    assert Day19.part_1_shortcurt(3014603) == 1834903
+  end
 end
