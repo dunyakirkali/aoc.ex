54be67dcbcb4b39f269a50b6574cf3eab31014e4
Dunya Kirkali <dunyakirkali@gmail.com>
2019-10-25 09:46:52 +0200

    2016 Day 19 Part 2

--- 2016/day19/lib/day19.ex
+++ 2016/day19/lib/day19.ex
@@ -17,6 +17,58 @@ defmodule Day19 do
   end
 
   @doc """
+      iex> Day19.part_2(1)
+      1
+      iex> Day19.part_2(2)
+      1
+      iex> Day19.part_2(3)
+      3
+      iex> Day19.part_2(4)
+      1
+      iex> Day19.part_2(5)
+      2
+      iex> Day19.part_2(6)
+      3
+  """
+  def part_2(elf_count) do
+    0..(elf_count - 1)
+    |> Enum.to_list()
+    |> do_part_2(0)
+  end
+
+  def part_2_shortcut(elf_count) do
+    last = find_greatest_multiplier(elf_count, 3) |> IO.inspect
+    elf_count - last
+  end
+
+  defp find_greatest_multiplier(elf_count, last_known) when last_known * 3 > elf_count, do: last_known
+  defp find_greatest_multiplier(elf_count, last_known) do
+    find_greatest_multiplier(elf_count, last_known * 3)
+  end
+
+  defp do_part_2(elves, index) do
+    if Enum.count(elves) < 2 do
+      List.first(elves) + 1
+    else
+      curr = Enum.at(elves, index)
+      # index |> IO.inspect(label: "Index")
+      jump =
+        elves
+        # |> IO.inspect(label: "Elves")
+        |> Enum.count()
+        |> div(2)
+        |> round()
+      across = rem(jump + index, Enum.count(elves))
+      # |> IO.inspect(label: "Across")
+      elves = List.delete_at(elves, across)
+      # |> IO.inspect
+      corrected_index = Enum.find_index(elves, fn x -> x == curr end)
+
+      do_part_2(elves, rem(corrected_index + 1, Enum.count(elves)))
+    end
+  end
+
+  @doc """
       iex> Day19.part_1_shortcurt(5)
       3
   """
--- 2016/day19/test/day19_test.exs
+++ 2016/day19/test/day19_test.exs
@@ -2,11 +2,29 @@ defmodule Day19Test do
   use ExUnit.Case
   doctest Day19
 
-  test "Part 1" do
-    assert Day19.part_1(3014603) == 1834903
-  end
+  # test "Part 1" do
+  #   assert Day19.part_1(3014603) == 1834903
+  # end
 
   test "Part 1 shortcut" do
-    assert Day19.part_1_shortcurt(3014603) == 1834903
+    assert Day19.part_1_shortcurt(3_014_603) == 1_834_903
+  end
+
+  # test "Part 2 analysis" do
+  #   # IO.puts(rem(3014603, 3))
+  #   # IO.puts(rem(3014601, 3))
+  #   1..3014603
+  #   |> Enum.each(fn elf_count ->
+  #     res = Day19.part_2(elf_count)
+  #
+  #     tri = Integer.to_string(elf_count, 3)
+  #     if elf_count == res or res == 1 do
+  #       IO.puts("Count: #{elf_count} (#{tri})=> #{res}")
+  #     end
+  #   end)
+  # end
+
+  test "Part 2 shortcut" do
+    assert Day19.part_2_shortcut(3_014_603) == 1_420_280
   end
 end
