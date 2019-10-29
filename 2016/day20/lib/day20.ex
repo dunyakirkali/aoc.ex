8417a06c7caa8600a42880dfdfbd4440f36d823b
Dunya Kirkali <dunyakirkali@gmail.com>
2019-10-29 21:12:38 +0100

    2016 Day 20 Part 1

--- 2016/day20/lib/day20.ex
+++ 2016/day20/lib/day20.ex
@@ -1,10 +1,6 @@
 defmodule Day20 do
-  @doc """
-      iex> Day20.part_1("priv/example.txt")
-      3
-  """
-  def part_1(file) do
-    [_..first, last.._] = 
+  def part_1_shortcut(file) do
+    ranges =
       file
       |> File.read!
       |> String.split("\n", trim: true)
@@ -14,19 +10,75 @@ defmodule Day20 do
           |> String.split("-")
           |> Enum.sort
           |> Enum.map(&String.to_integer/1)
-        
+
         from..to
       end)
-      |> IO.inspect(label: "not - ordered")
-      |> Enum.sort
-      |> IO.inspect(label: "list ordered")
-      |> Enum.chunk_every(2, 1, :discard)
-      |> Enum.drop_while(fn [from_range_s..from_range_e, to_range_s..to_range_e] ->
-        from_range_e + 1 >= to_range_s
+
+    possibilities =
+      file
+      |> File.read!
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [from, to] =
+          line
+          |> String.split("-")
+          |> Enum.sort
+          |> Enum.map(&String.to_integer/1)
+
+        to + 1
       end)
-      |> List.first
-      |> IO.inspect(label: "-")
-    
-    first + 1
+      |> Enum.sort
+
+    possibilities
+    |> Enum.map(fn possibility ->
+      if Enum.all?(ranges, fn range -> !Enum.member?(range, possibility) end) do
+        possibility
+      else
+        nil
+      end
+    end)
+    |> Enum.filter(&(&1 != nil))
+    |> List.first
+  end
+
+  def part_1(file) do
+    file
+    |> File.read!
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [from, to] =
+        line
+        |> String.split("-")
+        |> Enum.sort
+        |> Enum.map(&String.to_integer/1)
+
+      from..to
+    end)
+    |> unite
+    |> lowest_valued_ip
+  end
+
+  @doc """
+      iex> Day20.lowest_valued_ip([0, 1, 2, 4, 5, 6, 7, 8])
+      3
+  """
+  def lowest_valued_ip(list) do
+    0..4294967295
+    |> Stream.drop_while(fn ip ->
+      Enum.member?(list, ip)
+    end)
+    |> Enum.at(0)
+  end
+
+  @doc """
+      iex> Day20.unite([5..8, 0..2, 4..7])
+      [0, 1, 2, 4, 5, 6, 7, 8]
+  """
+  def unite(ranges) do
+    ranges
+    |> Enum.reduce(MapSet.new, fn range, acc ->
+      MapSet.union(acc, MapSet.new(range))
+    end)
+    |> Enum.to_list
   end
 end
--- 2016/day20/test/day20_test.exs
+++ 2016/day20/test/day20_test.exs
@@ -2,7 +2,8 @@ defmodule Day20Test do
   use ExUnit.Case
   doctest Day20
 
+  @tag timeout: :infinity
   test "Part 1" do
-    assert Day20.part_1("priv/input.txt") == :world
+    assert Day20.part_1_shortcut("priv/input.txt") == 17348574
   end
 end
