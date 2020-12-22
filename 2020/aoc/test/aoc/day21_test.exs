a2b117f1b1cac458933f560735b3525e554a80c1
Dunya Kirkali <dunyakirkali@gmail.com>
2020-12-22 15:18:05 +0100

    Boyscout

--- 2020/aoc/test/aoc/day21_test.exs
+++ 2020/aoc/test/aoc/day21_test.exs
@@ -2,13 +2,13 @@ defmodule Day21Test do
   use ExUnit.Case
   doctest Aoc.Day21
 
-   test "Part 1" do
-     inp = Aoc.Day21.input("priv/day21/input.txt")
-     assert Aoc.Day21.part1(inp) == 2170
-   end
+  test "Part 1" do
+    inp = Aoc.Day21.input("priv/day21/input.txt")
+    assert Aoc.Day21.part1(inp) == 2170
+  end
 
-   test "Part 2" do
-     inp = Aoc.Day21.input("priv/day21/input.txt")
-     assert Aoc.Day21.part2(inp) == "nfnfk,nbgklf,clvr,fttbhdr,qjxxpr,hdsm,sjhds,xchzh"
-   end
+  test "Part 2" do
+    inp = Aoc.Day21.input("priv/day21/input.txt")
+    assert Aoc.Day21.part2(inp) == "nfnfk,nbgklf,clvr,fttbhdr,qjxxpr,hdsm,sjhds,xchzh"
+  end
 end
