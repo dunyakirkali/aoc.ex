7a181ece29e8b768724d92c727ebb38f5ee70d99
Dunya Kirkali <dunyakirkali@gmail.com>
2021-12-16 13:56:43 +0100

    2021 Day 16

---
+++ 2021/aoc/test/aoc/day16_test.exs
@@ -0,0 +1,14 @@
+defmodule Day16Test do
+  use ExUnit.Case
+  doctest Aoc.Day16
+
+  test "Part 1" do
+    input = Aoc.Day16.input("priv/day16/input.txt")
+    assert Aoc.Day16.part1(input) == 925
+  end
+
+  # test "Part 2" do
+  #   input = Aoc.Day16.input("priv/day16/input.txt")
+  #   assert Aoc.Day16.part2(input) == 342997120375
+  # end
+end
