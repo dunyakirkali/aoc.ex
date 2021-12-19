6826232202879a28dfa1451aae124bfd1f0bbfdd
Dunya Kirkali <dunyakirkali@gmail.com>
2021-12-19 14:58:45 +0100

    2021 Day 19

---
+++ 2021/aoc/lib/aoc/day19.ex
@@ -0,0 +1,174 @@
+defmodule Aoc.Day19 do
+  # @doc """
+  #     iex> input = Aoc.Day19.input("priv/day19/example.txt")
+  #     ...> Aoc.Day19.part1(input)
+  #     79
+  # """
+  # def part1(input) do
+  #   scanner_positions = %{
+  #     0 => {[0,0,0], 0}
+  #   }
+  #   beacons = Map.get(input, 0)
+
+  #   Stream.iterate(0, &(&1 + 1))
+  #   |> Enum.reduce_while({beacons, scanner_positions}, fn _i, {bs, sp} ->
+  #     if Enum.count(sp) == Enum.count(input) do
+  #       {:halt, {bs, Map.values(sp)}}
+  #     else
+
+  #       input
+  #       |> Map.keys()
+  #       |> Enum.filter(fn ind ->
+  #         not Enum.member?(sp, ind)
+  #       end)
+  #       |> Enum.reduce({bs, sp}, fn index, {bs, sp} ->
+  #         scans = Map.get(input, index)
+  #         Enum.reduce(sp, {bs, sp}, fn {i, position}, {bs, sp} ->
+  #           known_scans = convert(Map.get(input, i), position)
+
+  #           if scanner_position = find_orientation(known_scans) do
+  #             {[convert(scans, scanner_position) | bs] , Map.put(sp, index, scanner_position)}
+  #           else
+  #             {bs, sp}
+  #           end
+  #         end)
+  #       end)
+
+  #       {:cont, {bs, sp}}
+  #     end
+  #   end)
+  # end
+
+  def find_orientation(scans, known_scans) do
+    orientations
+    |> Enum.reduce_while({}, fn rotation, acc ->
+      scans = Enum.map(scans, fn scan ->
+
+      end)
+    end)
+  end
+
+  @doc """
+      # iex> input = Aoc.Day19.input("priv/day19/example.txt")
+      # ...> Aoc.Day19.part2(input)
+      # nil
+  """
+  def part2(input) do
+
+  end
+
+  @doc """
+      iex> Aoc.Day19.distance([2, 3, 4], [-4, -3, -2])
+      18
+  """
+  def distance([lx, ly, lz], [rx, ry, rz]) do
+    abs(lx - rx) + abs(ly - ry) + abs(lz - rz)
+  end
+
+  @doc """
+      iex> Aoc.Day19.convert([[1, 1, 1], [2, 2, 2]], {[2, 3, 4], 2})
+      [[1, 2, 5], [0, 1, 6]]
+  """
+  def convert(scans, {offset, rotation}) do
+    scans
+    |> Enum.map(fn scan ->
+      scan
+      |> orientations()
+      |> Enum.at(rotation)
+      |> add(offset)
+    end)
+  end
+
+  @doc """
+      iex> Aoc.Day19.add([2, 3, 4], [-4, -3, -2])
+      [-2, 0, 2]
+  """
+  def add([lx, ly, lz], [rx, ry, rz]) do
+    [lx + rx, ly + ry, lz + rz]
+  end
+
+  @doc """
+      iex> Aoc.Day19.sub([2, 3, 4], [-4, -3, -2])
+      [6, 6, 6]
+  """
+  def sub([lx, ly, lz], [rx, ry, rz]) do
+    [lx - rx, ly - ry, lz - rz]
+  end
+
+  @doc """
+      iex> Aoc.Day19.orientations([2, 3, 4])
+      [[2, 3, 4], [3, -2, 4], [-2, -3, 4], [-3, 2, 4], [-4, 3, 2], [3, 4, 2], [4, -3, 2], [-3, -4, 2], [-2, 3, -4], [3, 2, -4], [2, -3, -4], [-3, -2, -4], [4, 3, -2], [3, -4, -2], [-4, -3, -2], [-3, 4, -2], [2, 4, -3], [4, -2, -3], [-2, -4, -3], [-4, 2, -3], [2, -4, 3], [-4, -2, 3], [-2, 4, 3], [4, 2, 3]]
+  """
+  def orientations(pos) do
+    [
+      pos,
+      pos |> rotate(:z),
+      pos |> rotate(:z) |> rotate(:z),
+      pos |> rotate(:z) |> rotate(:z) |> rotate(:z),
+
+      pos |> rotate(:y),
+      pos |> rotate(:y) |> rotate(:z),
+      pos |> rotate(:y) |> rotate(:z) |> rotate(:z),
+      pos |> rotate(:y) |> rotate(:z) |> rotate(:z) |> rotate(:z),
+
+      pos |> rotate(:y) |> rotate(:y),
+      pos |> rotate(:y) |> rotate(:y) |> rotate(:z),
+      pos |> rotate(:y) |> rotate(:y) |> rotate(:z) |> rotate(:z),
+      pos |> rotate(:y) |> rotate(:y) |> rotate(:z) |> rotate(:z) |> rotate(:z),
+
+      pos |> rotate(:y) |> rotate(:y) |> rotate(:y),
+      pos |> rotate(:y) |> rotate(:y) |> rotate(:y) |> rotate(:z),
+      pos |> rotate(:y) |> rotate(:y) |> rotate(:y) |> rotate(:z) |> rotate(:z),
+      pos |> rotate(:y) |> rotate(:y) |> rotate(:y) |> rotate(:z) |> rotate(:z) |> rotate(:z),
+
+      pos |> rotate(:x),
+      pos |> rotate(:x) |> rotate(:z),
+      pos |> rotate(:x) |> rotate(:z) |> rotate(:z),
+      pos |> rotate(:x) |> rotate(:z) |> rotate(:z) |> rotate(:z),
+
+      pos |> rotate(:x) |> rotate(:x) |> rotate(:x),
+      pos |> rotate(:x) |> rotate(:x) |> rotate(:x) |> rotate(:z),
+      pos |> rotate(:x) |> rotate(:x) |> rotate(:x) |> rotate(:z) |> rotate(:z),
+      pos |> rotate(:x) |> rotate(:x) |> rotate(:x) |> rotate(:z) |> rotate(:z) |> rotate(:z),
+    ]
+  end
+
+  @doc """
+      iex> Aoc.Day19.rotate([2, 3, 4], :x)
+      [2, 4, -3]
+
+      iex> Aoc.Day19.rotate([2, 3, 4], :y)
+      [-4, 3, 2]
+
+      iex> Aoc.Day19.rotate([2, 3, 4], :z)
+      [3, -2, 4]
+  """
+  def rotate([x, y, z], :x), do: [x, z, -y]
+  def rotate([x, y, z], :y), do: [-z, y, x]
+  def rotate([x, y, z], :z), do: [y, -x, z]
+
+  def input(filename) do
+    filename
+    |> File.read!()
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn scanner ->
+      [h | t] =
+        scanner
+        |> String.split("\n", trim: true)
+
+      <<"--- scanner ", sno ," ---">> = h
+      coords =
+        t
+        |> Enum.map(fn line ->
+          line
+          |> String.split(",", trim: true)
+          |> Enum.map(fn cor ->
+            String.to_integer(cor)
+          end)
+        end)
+
+      {sno - ?0, coords}
+    end)
+    |> Enum.into(%{})
+  end
+end
---
+++ 2021/aoc/test/aoc/day19_test.exs
@@ -0,0 +1,14 @@
+defmodule Day19Test do
+  use ExUnit.Case
+  doctest Aoc.Day19
+
+  # test "Part 1" do
+  #   input = Aoc.Day19.input("priv/day19/input.txt")
+  #   assert Aoc.Day19.part1(input) == nil
+  # end
+
+  # test "Part 2" do
+  #   input = Aoc.Day19.input("priv/day19/input.txt")
+  #   assert Aoc.Day19.part2(input) == nil
+  # end
+end
