64cde2e8775e59fc8974c58d9c945c86294e23ed
dunyakirkali <dunyakirkali@gmail.com>
2022-12-04 10:36:37 +0100

    Format

--- 2022/aoc/lib/aoc/day2.ex
+++ 2022/aoc/lib/aoc/day2.ex
@@ -8,12 +8,14 @@ defmodule Aoc.Day2 do
             :p -> 6
             :s -> 0
           end
+
         :p ->
           case you do
             :r -> 0
             :p -> 3
             :s -> 6
           end
+
         :s ->
           case you do
             :r -> 6
@@ -39,12 +41,14 @@ defmodule Aoc.Day2 do
             "Y" -> :r
             "Z" -> :p
           end
+
         :p ->
           case exp do
             "X" -> :r
             "Y" -> :p
             "Z" -> :s
           end
+
         :s ->
           case exp do
             "X" -> :p
@@ -68,12 +72,14 @@ defmodule Aoc.Day2 do
           "B" -> :p
           "C" -> :s
         end
+
       you =
         case you do
           "X" -> :r
           "Y" -> :p
           "Z" -> :s
         end
+
       {opp, you}
     end)
     |> Enum.map(fn move ->
@@ -82,7 +88,7 @@ defmodule Aoc.Day2 do
         RPS.score_move(you) + RPS.score_game(move)
       end)
     end)
-    |> Enum.sum
+    |> Enum.sum()
   end
 
   @doc """
@@ -98,6 +104,7 @@ defmodule Aoc.Day2 do
           "B" -> :p
           "C" -> :s
         end
+
       you = RPS.decide_move({opp, exp})
       {opp, you}
     end)
@@ -107,13 +114,13 @@ defmodule Aoc.Day2 do
         RPS.score_move(you) + RPS.score_game(move)
       end)
     end)
-    |> Enum.sum
+    |> Enum.sum()
   end
 
   def input() do
     "priv/day2/input.txt"
     |> File.read!()
     |> String.split("\n", trim: true)
-    |> Enum.map(&(String.split(&1, " ", trim: true)))
+    |> Enum.map(&String.split(&1, " ", trim: true))
   end
 end
