4765f0a7740987500ac6052afe0c0275a10275b5
dunyakirkali <dunyakirkali@gmail.com>
2022-12-20 21:22:51 +0100

    2022 Dy 20 Take 3

--- 2022/aoc/lib/aoc/day20.ex
+++ 2022/aoc/lib/aoc/day20.ex
@@ -10,28 +10,28 @@ defmodule Aoc.Day20 do
     # end)
   end
 
-	def left_rotate(l, n \\ 1)
-	def left_rotate([], _), do: []
-	def left_rotate(l, 0), do: l
-	def left_rotate([h | t], 1), do: t ++ [h]
-	def left_rotate(l, n) when n > 0, do: left_rotate(left_rotate(l, 1), n-1)
-	def left_rotate(l, n), do: right_rotate(l, -n)
+  def left_rotate(l, n \\ 1)
+  def left_rotate([], _), do: []
+  def left_rotate(l, 0), do: l
+  def left_rotate([h | t], 1), do: t ++ [h]
+  def left_rotate(l, n) when n > 0, do: left_rotate(left_rotate(l, 1), n - 1)
+  def left_rotate(l, n), do: right_rotate(l, -n)
 
-	def right_rotate(l, n \\ 1)
-	def right_rotate(l, n) when n > 0, do: Enum.reverse(l) |> left_rotate(n) |> Enum.reverse
-	def right_rotate(l, n), do: left_rotate(l, -n)
+  def right_rotate(l, n \\ 1)
+  def right_rotate(l, n) when n > 0, do: Enum.reverse(l) |> left_rotate(n) |> Enum.reverse()
+  def right_rotate(l, n), do: left_rotate(l, -n)
 
-  @doc """
-      iex> "priv/day20/example.txt" |> Aoc.Day20.input() |> Aoc.Day20.part1()
-      3
-  """
+  # @doc """
+  #     iex> "priv/day20/example.txt" |> Aoc.Day20.input() |> Aoc.Day20.part1()
+  #     3
+  # """
   def part1(numbers) do
     len = Enum.count(numbers)
-    indices = for x <- 0..len-1, do: x
+    indices = for x <- 0..(len - 1), do: x
 
     indices
     |> Enum.reduce({numbers, indices}, fn index, {numbers, indices} ->
-      IO.puts("#{index/len}")
+      # IO.puts("#{index/len}")
       location = Enum.find_index(indices, fn x -> x == index end)
 
       numbers = left_rotate(numbers, location)
@@ -43,8 +43,8 @@ defmodule Aoc.Day20 do
       numbers = left_rotate(numbers, number)
       indices = left_rotate(indices, number)
 
-      {[number | numbers], [index | indices]}# |> IO.inspect()
-
+      # |> IO.inspect()
+      {[number | numbers], [index | indices]}
     end)
     |> elem(0)
     |> then(fn result ->
@@ -58,14 +58,47 @@ defmodule Aoc.Day20 do
     |> Enum.sum()
   end
 
-  # @doc """
-  #     iex> "priv/day20/example.txt" |> Aoc.Day20.input() |> Aoc.Day20.part2()
-  #     3472
-  # """
-  # def part2(blueprints) do
-  #   blueprints
-  #   |> Enum.take(3)
-  #   |> Aoc.Parallel.pmap(&solve(&1, 32))
-  #   |> Enum.reduce(1, fn x, acc -> x * acc end)
-  # end
+  @doc """
+      iex> "priv/day20/example.txt" |> Aoc.Day20.input() |> Aoc.Day20.part2()
+      1623178306
+  """
+  def part2(numbers) do
+    numbers = Enum.map(numbers, fn num -> 811_589_153 * num end)
+    len = Enum.count(numbers)
+    indices = for x <- 0..(len - 1), do: x
+
+    0..9
+    |> Enum.reduce({numbers, indices}, fn inc, {numbers, indices} ->
+      IO.puts("loop #{inc}")
+      indices
+      |> Enum.reduce({numbers, indices}, fn index, {numbers, indices} ->
+        IO.puts("#{index/len}")
+        location = Enum.find_index(indices, fn x -> x == index end)
+
+        numbers = left_rotate(numbers, location)
+        indices = left_rotate(indices, location)
+
+        [number | numbers] = numbers
+        [index | indices] = indices
+
+        number_shift = rem(number, (len - 1))
+
+        numbers = left_rotate(numbers, number_shift)
+        indices = left_rotate(indices, number_shift)
+
+        # |> IO.inspect()
+        {[number | numbers], [index | indices]}
+      end)
+    end)
+    |> elem(0)
+    |> then(fn result ->
+      zindex = Enum.find_index(result, fn x -> x == 0 end)
+
+      [1000, 2000, 3000]
+      |> Enum.map(fn index ->
+        Enum.at(result, rem(zindex + index, len))
+      end)
+    end)
+    |> Enum.sum()
+  end
 end
--- 2022/aoc/test/aoc/day20_test.exs
+++ 2022/aoc/test/aoc/day20_test.exs
@@ -2,13 +2,13 @@ defmodule Day20Test do
   use ExUnit.Case
   doctest Aoc.Day20
 
-  test "Part 1" do
-    input = Aoc.Day20.input("priv/day20/input.txt")
-    assert Aoc.Day20.part1(input) == 10707
-  end
+  # test "Part 1" do
+  #   input = Aoc.Day20.input("priv/day20/input.txt")
+  #   assert Aoc.Day20.part1(input) == 10707
+  # end
 
   # test "Part 2" do
   #   input = Aoc.Day20.input("priv/day20/input.txt")
-  #   assert Aoc.Day20.part2(input) == 0
+  #   assert Aoc.Day20.part2(input) == 2488332343098
   # end
 end
