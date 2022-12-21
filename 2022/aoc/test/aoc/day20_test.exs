cbae3f9b01434e6135c97b2ab5b60481b158abe1
dunyakirkali <dunyakirkali@gmail.com>
2022-12-21 17:04:15 +0100

    Mix format

--- 2022/aoc/test/aoc/day20_test.exs
+++ 2022/aoc/test/aoc/day20_test.exs
@@ -7,8 +7,8 @@ defmodule Day20Test do
     assert Aoc.Day20.part1(input) == 10707
   end
 
-  # test "Part 2" do
-  #   input = Aoc.Day20.input("priv/day20/input.txt")
-  #   assert Aoc.Day20.part2(input) == 2488332343098
-  # end
+  test "Part 2" do
+    input = Aoc.Day20.input("priv/day20/input.txt")
+    assert Aoc.Day20.part2(input) == 2_488_332_343_098
+  end
 end
