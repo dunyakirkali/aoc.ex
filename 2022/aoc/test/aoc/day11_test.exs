42ad69a55a46406634b5bf69fb0b7dd50193089b
dunyakirkali <dunyakirkali@gmail.com>
2022-12-26 00:17:51 +0100

    Clean up

---
+++ 2022/aoc/test/aoc/day11_test.exs
@@ -0,0 +1,14 @@
+defmodule Day11Test do
+  use ExUnit.Case
+  doctest Aoc.Day11
+
+  # test "Part 1" do
+  #   input = Aoc.Day11.input("priv/day11/input.txt")
+  #   assert Aoc.Day11.part1(input) == 13920
+  # end
+
+  # test "Part 2" do
+  #   input = Aoc.Day11.input("priv/day11/input.txt")
+  #   assert Aoc.Day11.part2(input) == :ok
+  # end
+end
---
+++ 2022/aoc/test/aoc/day24_test.exs
@@ -0,0 +1,14 @@
+defmodule Day24Test do
+  use ExUnit.Case
+  doctest Aoc.Day24
+
+  # test "Part 1" do
+  #   input = Aoc.Day24.input("priv/day24/input.txt")
+  #   assert Aoc.Day24.part1(input) == 4302
+  # end
+
+  # test "Part 2" do
+  #   input = Aoc.Day24.input("priv/day24/input.txt")
+  #   assert Aoc.Day24.part2(input) == 1025
+  # end
+end
