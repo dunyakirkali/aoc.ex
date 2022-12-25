92adbdf8fa09ee53e59e80bd623bda3ece0110d2
dunyakirkali <dunyakirkali@gmail.com>
2022-12-25 10:02:02 +0100

    Clean up

--- 2022/aoc/lib/aoc/day1.ex
+++ 2022/aoc/lib/aoc/day1.ex
@@ -1,34 +1,35 @@
 defmodule Aoc.Day1 do
   @doc """
-      iex> Aoc.Day1.part1([6000, 4000, 11000, 24000, 10000])
+      iex> "priv/day1/example.txt" |> Aoc.Day1.input() |> Aoc.Day1.part1()
       24000
   """
   def part1(list) do
     list
+    |> Enum.map(&Enum.sum/1)
     |> Enum.max()
   end
 
   @doc """
-      iex> Aoc.Day1.part2([6000, 4000, 11000, 24000, 10000])
+      iex> "priv/day1/example.txt" |> Aoc.Day1.input() |> Aoc.Day1.part2()
       45000
   """
   def part2(list) do
     list
+    |> Enum.map(&Enum.sum/1)
     |> Enum.sort()
     |> Enum.reverse()
     |> Enum.take(3)
     |> Enum.sum()
   end
 
-  def input() do
-    "priv/day1/input.txt"
+  def input(filename) do
+    filename
     |> File.read!()
     |> String.split("\n\n", trim: true)
     |> Enum.map(fn elf ->
       elf
       |> String.split("\n", trim: true)
       |> Enum.map(&String.to_integer/1)
-      |> Enum.sum()
     end)
   end
 end
--- 2022/aoc/test/aoc/day1_test.exs
+++ 2022/aoc/test/aoc/day1_test.exs
@@ -3,12 +3,12 @@ defmodule Day1Test do
   doctest Aoc.Day1
 
   test "Part 1" do
-    input = Aoc.Day1.input()
+    input = Aoc.Day1.input("priv/day1/input.txt")
     assert Aoc.Day1.part1(input) == 68787
   end
 
   test "Part 2" do
-    input = Aoc.Day1.input()
+    input = Aoc.Day1.input("priv/day1/input.txt")
     assert Aoc.Day1.part2(input) == 198_041
   end
 end
