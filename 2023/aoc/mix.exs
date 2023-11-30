59735a00926374456e5a7b03701f40e989a5fb6c
Dunya Kirkali <dunya.kirkali@booking.com>
2023-11-30 20:17:29 +0100

    Add dependencies

--- 2023/aoc/mix.exs
+++ 2023/aoc/mix.exs
@@ -21,8 +21,26 @@ defmodule Aoc.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:libgraph, "~> 0.13.3"},
+      {:nimble_parsec, "~> 1.2.0"},
+      {:memoize, "~> 1.2"},
+      {:benchee, "~> 1.0"},
+      {:benchee_html, "~> 1.0", only: [:dev, :test]},
+      {:printex, "~> 1.1.0"},
+      {:drawille, "~> 0.0.1"},
+      {:distance, "~> 0.2.1"},
+      {:poison, "~> 5.0.0"},
+      {:comb, git: "https://github.com/tallakt/comb.git"},
+      {:tensor, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:exprof, "~> 0.2.0"},
+      {:flow, "~> 1.1.0"},
+      {:deque, "~> 1.0"},
+      {:cll, "~> 0.1.0"},
+      {:genex, "~> 1.0.0-beta"},
+      {:complex, "~> 0.4.1"},
+      {:nx, "~> 0.2"},
+      {:math, "~> 0.6.0"}
     ]
   end
 end
