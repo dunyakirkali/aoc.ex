f3879d035d3644a3f186bf6eecf390b8e2624606
Dunya Kirkali <dunya.kirkali@booking.com>
2023-12-01 06:07:55 +0100

    one

--- 2023/aoc/lib/aoc/day1.ex
+++ 2023/aoc/lib/aoc/day1.ex
@@ -5,31 +5,42 @@ defmodule Aoc.Day1 do
   """
   def part1(list) do
     list
-    |> Enum.map(&Enum.sum/1)
-    |> Enum.max()
+    |>IO.inspect()
+		|> Enum.sum()
   end
 
-  # @doc """
-  #     iex> "priv/day1/example.txt" |> Aoc.Day1.input() |> Aoc.Day1.part2()
-  #     45000
-  # """
-  # def part2(list) do
-  #   list
-  #   |> Enum.map(&Enum.sum/1)
-  #   |> Enum.sort()
-  #   |> Enum.reverse()
-  #   |> Enum.take(3)
-  #   |> Enum.sum()
-  # end
+  @doc """
+      iex> "priv/day1/example.txt" |> Aoc.Day1.input() |> Aoc.Day1.part2()
+      45000
+  """
+  def part2(list) do
+    list
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.sort()
+    |> Enum.reverse()
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
 
   def input(filename) do
     filename
     |> File.read!()
-    |> String.split("\n\n", trim: true)
-    |> Enum.map(fn elf ->
-      elf
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.to_integer/1)
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      numbers =
+				line
+				|> String.graphemes()
+				|> Enum.map(fn x ->
+					case Integer.parse(x) do
+						{num, _} -> num
+						_ -> nil
+					end
+				end)
+				|> Enum.filter(& !is_nil(&1))
+			fi = List.first(numbers)
+			la = List.last(numbers)
+
+			[fi, la]|> Integer.undigits()
     end)
   end
 end
