702e16a3351ff06065daaa8cbfc715be7eaccc69
Dunya Kirkali <dunya.kirkali@booking.com>
2023-12-05 13:22:23 +0100

    Clean up

--- 2023/aoc/lib/aoc/day5.ex
+++ 2023/aoc/lib/aoc/day5.ex
@@ -1,6 +1,4 @@
 defmodule Aoc.Day5 do
-  use Memoize
-
   @doc """
       iex> "priv/day5/example.txt" |> Aoc.Day5.input() |> Aoc.Day5.part1()
       35
@@ -10,86 +8,11 @@ defmodule Aoc.Day5 do
     |> Enum.min()
   end
 
-  def aert(x, rules) do
-    {:cond, [],
-     [
-       [
-         {:do,
-          Enum.map(rules, fn [dr, sr] ->
-            condition(Enum.at(dr, 0), Enum.at(sr, 0), Enum.count(dr), x)
-          end)}
-       ]
-     ]}
-  end
-
   @doc """
       iex> "priv/day5/example.txt" |> Aoc.Day5.input() |> Aoc.Day5.part2()
       46
   """
   def part2({seeds, rules}) do
-    aert(42, Map.get(rules, "seed-to-soil"))
-    |> Macro.to_string()
-    |> IO.inspect()
-
-    # condition(50, 98, 2, 99)
-    # trea(51)
-    # |> IO.inspect()
-    # |> Macro.to_string()
-    # |> IO.inspect(label: "AST")
-
-    # |> Code.eval_quoted()
-
-    # |> IO.inspect(label: "AST")
-    # |> Code.eval_quoted()
-    # |> IO.inspect(label: "Eval")
-
-    seeds
-    |> seed_ranges()
-    |> Enum.count()
-
-    # |> Stream.flat_map(fn v ->
-    #   grow(rules, "seed", [v])
-    # end)
-    # |> Enum.min()
-  end
-
-  def condition(drs, srs, rl, val) do
-    {:->, [],
-     [
-       [
-         {:and, [context: Aoc.Day5, imports: [{2, Kernel}]],
-          [
-            {:<=, [context: Aoc.Day5, imports: [{2, Kernel}]],
-             to_charlist([srs, {val, [], Aoc.Day5}])},
-            {:<, [context: Aoc.Day5, imports: [{2, Kernel}]],
-             to_charlist([{val, [], Aoc.Day5}, srs + rl])}
-          ]}
-       ],
-       {:+, [context: Aoc.Day5, imports: [{1, Kernel}, {2, Kernel}]],
-        to_charlist([{val, [], Aoc.Day5}, drs - srs])}
-     ]}
-  end
-
-  def trea(x) do
-    quote do
-      cond do
-        98 <= unquote(x) and unquote(x) < 100 ->
-          unquote(x) - 48
-
-        50 <= unquote(x) and unquote(x) < 52 ->
-          unquote(x) + 2
-
-        true ->
-          unquote(x)
-      end
-    end
-  end
-
-  def seed_ranges(seeds) do
-    seeds
-    |> Stream.chunk_every(2)
-    |> Stream.flat_map(fn [sr, len] -> Range.new(sr, sr + len - 1) |> Enum.to_list() end)
-    |> Stream.uniq()
   end
 
   def fkey("seed"), do: "seed-to-soil"
@@ -111,8 +34,6 @@ defmodule Aoc.Day5 do
   def grow(_rules, "location", vals), do: vals
 
   def grow(rules, state, vals) do
-    state |> IO.inspect(label: "state")
-
     tr =
       Map.get(rules, fkey(state))
 
@@ -125,7 +46,7 @@ defmodule Aoc.Day5 do
     grow(rules, next(state), vals)
   end
 
-  defmemo calc(tr, val) do
+  def calc(tr, val) do
     fi =
       Enum.find_index(tr, fn trr ->
         source = Enum.at(trr, 1)
@@ -153,7 +74,6 @@ defmodule Aoc.Day5 do
       |> Enum.at(1)
       |> String.split(" ")
       |> Enum.map(fn ina -> String.to_integer(ina) end)
-      |> IO.inspect(label: "seeds")
 
     rules = List.delete_at(groups, 0)
 
