50ad9396c2b6ea96e15d4617997a9a32caf39ecd
Dunya Kirkali <dunya.kirkali@booking.com>
2023-12-06 06:33:55 +0100

    2023 Day 6

--- 2023/aoc/lib/aoc/day6.ex
+++ 2023/aoc/lib/aoc/day6.ex
@@ -1,21 +1,57 @@
 defmodule Aoc.Day6 do
   @doc """
       iex> "priv/day6/example.txt" |> Aoc.Day6.input() |> Aoc.Day6.part1()
-      35
+      288
   """
   def part1(input) do
-    input
+    calc(input)
   end
 
-  # @doc """
-  #     iex> "priv/day6/example.txt" |> Aoc.Day6.input() |> Aoc.Day6.part2()
-  #     46
-  # """
-  # def part2({seeds, rules}) do
-  # end
-  def input(filename) do
-    filename
-    |> File.read!()
-    |> String.split("\n\n", trim: true)
+  def calc(input) do
+    input
+    |> Enum.map(fn {_race, {time, distance}} ->
+      Stream.iterate(0, &(&1 + 1))
+      |> Enum.take_while(fn x -> x <= time end)
+      |> Stream.filter(fn wait ->
+        wait + wait * (time - wait - 1) > distance
+      end)
+      |> Enum.to_list()
+    end)
+    |> Enum.map(&Enum.count/1)
+    |> Enum.reduce(1, fn count, acc -> count * acc end)
+  end
+
+  @doc """
+      iex> "priv/day6/example.txt" |> Aoc.Day6.input(:part2) |> Aoc.Day6.part2()
+      71503
+  """
+  def part2(input) do
+    calc(input)
+  end
+
+  def input(filename, part \\ :part1) do
+    secas =
+      filename
+      |> File.read!()
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [_, vs] = String.split(line, ~r/:\s+/)
+
+        if part == :part1 do
+          String.split(vs, ~r/\s+/)
+          |> Enum.map(&String.to_integer/1)
+        else
+          [
+            String.replace(vs, " ", "")
+            |> String.to_integer()
+          ]
+        end
+      end)
+
+    0..(Enum.count(Enum.at(secas, 0)) - 1)
+    |> Enum.to_list()
+    |> Enum.reduce(%{}, fn i, acc ->
+      Map.put(acc, i, {Enum.at(Enum.at(secas, 0), i), Enum.at(Enum.at(secas, 1), i)})
+    end)
   end
 end
--- 2023/aoc/test/aoc/day6_test.exs
+++ 2023/aoc/test/aoc/day6_test.exs
@@ -2,14 +2,13 @@ defmodule Day6Test do
   use ExUnit.Case
   doctest Aoc.Day6
 
-  # test "Part 1" do
-  #   input = Aoc.Day6.input("priv/day6/input.txt")
-  #   assert Aoc.Day6.part1(input) == 331_445_006
-  # end
+  test "Part 1" do
+    input = Aoc.Day6.input("priv/day6/input.txt")
+    assert Aoc.Day6.part1(input) == 2_065_338
+  end
 
-  #
-  # test "Part 2" do
-  #   input = Aoc.Day6.input("priv/day6/input.txt")
-  #   assert Aoc.Day6.part2(input) == 5_571_760
-  # end
+  test "Part 2" do
+    input = Aoc.Day6.input("priv/day6/input.txt", :part2)
+    assert Aoc.Day6.part2(input) == 34_934_171
+  end
 end
