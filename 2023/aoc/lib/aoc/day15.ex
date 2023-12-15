55fa57e8b31acea98c6cd8e1423e663006c00fdf
Dunya Kirkali <dunya.kirkali@booking.com>
2023-12-15 13:41:04 +0100

    2023 Day 15 Part 2

--- 2023/aoc/lib/aoc/day15.ex
+++ 2023/aoc/lib/aoc/day15.ex
@@ -6,7 +6,74 @@ defmodule Aoc.Day15 do
   def part1(map) do
     map
     |> Enum.map(fn piece -> hash(piece) end)
-    |> IO.inspect()
+    |> Enum.sum()
+  end
+
+  @doc """
+      iex> "priv/day15/example.txt" |> Aoc.Day15.input() |> Aoc.Day15.part2()
+      145
+  """
+  def part2(map) do
+    boxes =
+      0..255
+      |> Enum.reduce(%{}, fn i, acc ->
+        Map.put(acc, i, [])
+      end)
+
+    map
+    |> Enum.map(fn piece ->
+      cond do
+        String.match?(piece, ~r/.+=\d/) ->
+          [label, focal_length] =
+            String.split(piece, "=", trim: true)
+
+          {:set, label, hash(label), String.to_integer(focal_length)}
+
+        String.match?(piece, ~r/.+-/) ->
+          label = String.replace(piece, "-", "")
+          {:remove, label, hash(label)}
+      end
+    end)
+    |> Enum.reduce(boxes, fn action, acc ->
+      {_, acc} =
+        case action do
+          {:remove, label, box} ->
+            Map.get_and_update(acc, box, fn current_value ->
+              {current_value,
+               Enum.filter(current_value, fn {lbl, _} ->
+                 label != lbl
+               end)}
+            end)
+
+          {:set, label, box, focal_length} ->
+            Map.get_and_update(acc, box, fn current_value ->
+              if Enum.filter(current_value, fn {lbl, _} -> label == lbl end)
+                 |> Enum.count()
+                 |> Kernel.==(1) do
+                {current_value,
+                 Enum.map(current_value, fn {lbl, fl} ->
+                   if lbl == label do
+                     {lbl, focal_length}
+                   else
+                     {lbl, fl}
+                   end
+                 end)}
+              else
+                {current_value, current_value ++ [{label, focal_length}]}
+              end
+            end)
+        end
+
+      acc
+    end)
+    |> Enum.map(fn {box, list} ->
+      list
+      |> Enum.with_index()
+      |> Enum.map(fn {{_, fl}, index} ->
+        (index + 1) * fl * (box + 1)
+      end)
+    end)
+    |> List.flatten()
     |> Enum.sum()
   end
 
@@ -30,6 +97,5 @@ defmodule Aoc.Day15 do
     |> File.read!()
     |> String.trim()
     |> String.split(",", trim: true)
-    |> IO.inspect()
   end
 end
--- 2023/aoc/test/aoc/day15_test.exs
+++ 2023/aoc/test/aoc/day15_test.exs
@@ -7,8 +7,8 @@ defmodule Day15Test do
     assert Aoc.Day15.part1(input) == 516_804
   end
 
-  # test "Part 2" do
-  #   input = Aoc.Day15.input("priv/day15/input.txt")
-  #   assert Aoc.Day15.part2(input) == 104619
-  # end
+  test "Part 2" do
+    input = Aoc.Day15.input("priv/day15/input.txt")
+    assert Aoc.Day15.part2(input) == 231_844
+  end
 end
