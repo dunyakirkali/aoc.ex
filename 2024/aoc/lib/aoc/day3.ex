2943b1c03273092b4a7f3739a3867b0a621f325c
Dunya Kirkali <dunyakirkali@gmail.com>
2024-12-03 06:20:06 +0100

    2024 Day 3

--- 2024/aoc/lib/aoc/day3.ex
+++ 2024/aoc/lib/aoc/day3.ex
@@ -1,23 +1,47 @@
 defmodule Aoc.Day3 do
   @doc """
       iex> "priv/day3/example.txt" |> Aoc.Day3.input() |> Aoc.Day3.part1()
-      2
+      161
   """
   def part1(list) do
-    list
+    Regex.scan(~r/mul\((\d{0,3}),(\d{0,3})\)/, list)
+    |> Enum.map(fn mul ->
+      Enum.drop(mul, 1)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.reduce(&Kernel.*/2)
+    end)
+    |> Enum.sum()
   end
 
-  # @doc """
-  #     iex> "priv/day3/example.txt" |> Aoc.Day3.input() |> Aoc.Day3.part2()
-  #     4
-  # """
-  # def part2(list) do
-  #   list
-  # end
+  @doc """
+      iex> "priv/day3/example2.txt" |> Aoc.Day3.input() |> Aoc.Day3.part2()
+      48
+  """
+  def part2(list) do
+    {_, count} =
+      Regex.scan(~r/(?:mul|do|don't)\((\d{0,3}),?(\d{0,3})?\)/, list)
+      |> Enum.reduce({true, 0}, fn cmd, {mul, sum} ->
+        case cmd do
+          ["do()", "", ""] ->
+            {true, sum}
+
+          ["don't()", "", ""] ->
+            {false, sum}
+
+          [_, a, b] ->
+            if mul do
+              {mul, String.to_integer(a) * String.to_integer(b) + sum}
+            else
+              {mul, sum}
+            end
+        end
+      end)
+
+    count
+  end
 
   def input(filename) do
     filename
     |> File.read!()
-    |> String.split("\n", trim: true)
   end
 end
--- 2024/aoc/test/aoc/day3_test.exs
+++ 2024/aoc/test/aoc/day3_test.exs
@@ -4,11 +4,11 @@ defmodule Day3Test do
 
   test "Part 1" do
     input = Aoc.Day3.input("priv/day3/input.txt")
-    assert Aoc.Day3.part1(input) == 0
+    assert Aoc.Day3.part1(input) == 179_834_255
   end
 
   test "Part 2" do
     input = Aoc.Day3.input("priv/day3/input.txt")
-    assert Aoc.Day3.part2(input) == 514
+    assert Aoc.Day3.part2(input) == 80_570_939
   end
 end
