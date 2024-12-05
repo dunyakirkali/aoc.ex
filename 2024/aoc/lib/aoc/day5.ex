a4e1033c349b07e2bb5602a305574cb6511d8775
Dunya Kirkali <dunyakirkali@gmail.com>
2024-12-05 09:58:41 +0100

    Clean up

--- 2024/aoc/lib/aoc/day5.ex
+++ 2024/aoc/lib/aoc/day5.ex
@@ -8,41 +8,42 @@ defmodule Aoc.Day5 do
 
     lists
     |> Enum.filter(&valid?(rules, &1))
-
-    # |> Enum.map(fn list -> center(list) end)
-    # |> Enum.sum()
+    |> Enum.map(fn list -> center(list) end)
+    |> Enum.sum()
   end
 
-  # @doc """
-  #     iex> "priv/day5/example.txt" |> Aoc.Day5.input() |> Aoc.Day5.part2()
-  #     123
-  # """
-  # def part2(list) do
-  #   {rules, lists} = list
+  @doc """
+      iex> "priv/day5/example.txt" |> Aoc.Day5.input() |> Aoc.Day5.part2()
+      123
+  """
+  def part2(list) do
+    {rules, lists} = list
 
-  #   lists
-  #   |> Enum.filter(fn l ->
-  #     valid?(l, rules)
-  #     |> Kernel.not()
-  #   end)
-  #   |> pmap(fn il ->
-  #     il
-  #     |> Comb.permutations()
-  #     |> Stream.drop_while(fn comb ->
-  #       not valid?(comb, rules)
-  #     end)
-  #     |> Enum.at(0)
-  #   end)
-  #   |> Enum.map(fn list -> center(list) end)
-  #   |> Enum.sum()
-  # end
+    lists
+    |> Enum.filter(fn l ->
+      valid?(rules, l)
+      |> Kernel.not()
+    end)
+    |> Enum.map(fn l -> sort(rules, l) end)
+    |> Enum.map(fn list -> center(list) end)
+    |> Enum.sum()
+  end
 
   defp center(l) do
     Enum.at(l, div(length(l), 2))
   end
 
-  def valid?(_, []), do: true
-  def valid?(rules, [x | xs]), do: Enum.all?(xs, fn y -> y in rules[x] end) and valid?(rules, xs)
+  def sort(_, []), do: []
+
+  def sort(rules, current) do
+    next = Enum.find(current, fn x -> current -- Map.get(rules, x, []) == [x] end)
+    [next | sort(rules, List.delete(current, next))]
+  end
+
+  defp valid?(_, []), do: true
+
+  defp valid?(rules, [x | xs]),
+    do: Enum.all?(xs, fn y -> y in Map.get(rules, x, []) end) and valid?(rules, xs)
 
   def input(filename) do
     [h, t] =
