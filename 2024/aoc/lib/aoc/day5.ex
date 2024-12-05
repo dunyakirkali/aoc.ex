17602e60e67f99d77de876e0abcbe6a31be797d9
Dunya Kirkali <dunyakirkali@gmail.com>
2024-12-05 09:02:36 +0100

    2024 Day 5 Approach 1

--- 2024/aoc/lib/aoc/day5.ex
+++ 2024/aoc/lib/aoc/day5.ex
@@ -7,59 +7,42 @@ defmodule Aoc.Day5 do
     {rules, lists} = list
 
     lists
-    |> Enum.filter(fn l ->
-      valid?(l, rules)
-    end)
-    |> Enum.map(fn list -> center(list) end)
-    |> Enum.sum()
+    |> Enum.filter(&valid?(rules, &1))
+
+    # |> Enum.map(fn list -> center(list) end)
+    # |> Enum.sum()
   end
 
-  @doc """
-      iex> "priv/day5/example.txt" |> Aoc.Day5.input() |> Aoc.Day5.part2()
-      123
-  """
-  def part2(list) do
-    {rules, lists} = list
+  # @doc """
+  #     iex> "priv/day5/example.txt" |> Aoc.Day5.input() |> Aoc.Day5.part2()
+  #     123
+  # """
+  # def part2(list) do
+  #   {rules, lists} = list
 
-    lists
-    |> Enum.filter(fn l ->
-      valid?(l, rules)
-      |> Kernel.not()
-    end)
-    |> pmap(fn il ->
-      il
-      |> Comb.permutations()
-      |> Stream.drop_while(fn comb ->
-        not valid?(comb, rules)
-      end)
-      |> Enum.at(0)
-    end)
-    |> Enum.map(fn list -> center(list) end)
-    |> Enum.sum()
-  end
+  #   lists
+  #   |> Enum.filter(fn l ->
+  #     valid?(l, rules)
+  #     |> Kernel.not()
+  #   end)
+  #   |> pmap(fn il ->
+  #     il
+  #     |> Comb.permutations()
+  #     |> Stream.drop_while(fn comb ->
+  #       not valid?(comb, rules)
+  #     end)
+  #     |> Enum.at(0)
+  #   end)
+  #   |> Enum.map(fn list -> center(list) end)
+  #   |> Enum.sum()
+  # end
 
   defp center(l) do
     Enum.at(l, div(length(l), 2))
   end
 
-  def pmap(collection, func) do
-    collection
-    |> Enum.map(&Task.async(fn -> func.(&1) end))
-    |> Enum.map(fn x ->
-      Task.await(x, 1_000_000_000)
-    end)
-  end
-
-  defp valid?(l, rules) do
-    l
-    |> Stream.chunk_every(2, 1, :discard)
-    |> Stream.map(fn [from, to] ->
-      p = Graph.get_shortest_path(rules, from, to)
-
-      p != nil and Enum.count(p) == 2
-    end)
-    |> Enum.all?()
-  end
+  def valid?(_, []), do: true
+  def valid?(rules, [x | xs]), do: Enum.all?(xs, fn y -> y in rules[x] end) and valid?(rules, xs)
 
   def input(filename) do
     [h, t] =
@@ -71,9 +54,7 @@ defmodule Aoc.Day5 do
       h
       |> String.split("\n", trim: true)
       |> Enum.map(fn l -> String.split(l, "|", trim: true) |> Enum.map(&String.to_integer/1) end)
-      |> Enum.reduce(Graph.new(type: :directed), fn [from, to], acc ->
-        Graph.add_edge(acc, from, to)
-      end)
+      |> Enum.group_by(fn [key, _] -> key end, fn [_, value] -> value end)
 
     procedure =
       t
