e6096c6f81fc043223f1bc09f9fad3fd3febd380
Dunya Kirkali <dunyakirkali@gmail.com>
2024-12-17 05:52:34 +0100

    Optimize

--- 2024/aoc/README.md
+++ 2024/aoc/README.md
@@ -3,3 +3,4 @@
 [ ] Revisit Day 5
 [*] Revisit Day 7 and use DFS
 [ ] Day 12, undirected Graph doesn't work as expected
+[ ] Revisit Day 16, optimize
--- 2024/aoc/lib/aoc/day16.ex
+++ 2024/aoc/lib/aoc/day16.ex
@@ -2,105 +2,126 @@ defmodule Aoc.Day16 do
   use Memoize
 
   @doc """
-      iex> "priv/day16/example.txt" |> Aoc.Day16.input() |> Aoc.Day16.part1()
-      7036
+      # iex> "priv/day16/example.txt" |> Aoc.Day16.input() |> Aoc.Day16.part1()
+      # 7036
 
       iex> "priv/day16/example2.txt" |> Aoc.Day16.input() |> Aoc.Day16.part1()
       11048
   """
   def part1({start, desti, map}) do
-    map
-    |> to_graph()
-    |> find_all_paths(start, desti, :infinity)
-    |> Stream.map(fn path -> score(path, :infinity) end)
+    graph = to_graph(map)
+    queue = PriorityQueue.new() |> PriorityQueue.push({start, :right, []}, 0)
+    visited = MapSet.new([])
+
+    walk(graph, queue, visited, desti, [])
+    |> IO.inspect(limit: :infinity)
+    |> Enum.map(fn path -> score(path) end)
     |> Enum.min()
   end
 
-  def find_all_paths(graph, start, target, best_score) do
-    Stream.resource(
-      fn -> {[[start]], best_score} end,
-      fn
-        {[], _} ->
-          {:halt, []}
-
-        {[current_path | rest], best_score} ->
-          current_node = hd(current_path)
-          path = Enum.reverse(current_path)
-          current_score = score(path, best_score)
-
-          if current_score <= best_score do
-            cond do
-              current_node == target ->
-                if current_score <= best_score do
-                  {[path], {rest, current_score}}
-                else
-                  {[], {rest, best_score}}
-                end
-
-              true ->
-                neighbors =
-                  graph
-                  |> Graph.out_neighbors(current_node)
-                  |> Enum.reject(&(&1 in current_path))
-
-                new_paths =
-                  neighbors
-                  |> Enum.map(&[&1 | current_path])
-
-                {[], {rest ++ new_paths, best_score}}
-            end
-          else
-            {[], {rest, best_score}}
-          end
-      end,
-      fn _ -> :ok end
-    )
-  end
-
-  def score([_], best_score), do: best_score
-
-  defmemo score(path, _) do
-    path
-    |> Stream.chunk_every(2, 1, :discard)
-    |> Stream.transform({:right, 0}, fn
-      [{fx, fy}, {tx, ty}], {prev_dir, score} ->
-        new_dir =
-          case {tx - fx, ty - fy} do
-            {1, 0} -> :right
-            {-1, 0} -> :left
-            {0, 1} -> :down
-            {0, -1} -> :up
-          end
-
-        new_score = if(prev_dir == new_dir, do: score + 1, else: score + 1001)
-        {[new_score], {new_dir, new_score}}
-    end)
-    |> Enum.at(-1)
-  end
-
   @doc """
-      iex> "priv/day16/example.txt" |> Aoc.Day16.input() |> Aoc.Day16.part2()
-      45
+      # iex> "priv/day16/example.txt" |> Aoc.Day16.input() |> Aoc.Day16.part2()
+      # 45
 
-      iex> "priv/day16/example2.txt" |> Aoc.Day16.input() |> Aoc.Day16.part2()
-      64
+      # iex> "priv/day16/example2.txt" |> Aoc.Day16.input() |> Aoc.Day16.part2()
+      # 64
   """
   def part2({start, desti, map}) do
-    best_score = part1({start, desti, map})
+    graph = to_graph(map)
+    queue = PriorityQueue.new() |> PriorityQueue.push({start, :right, []}, 0)
+    visited = MapSet.new([])
 
-    map
-    |> to_graph()
-    |> find_all_paths(start, desti, best_score)
-    |> Enum.map(fn path -> MapSet.new(path) end)
-    |> Enum.reduce(MapSet.new(), fn path, acc ->
-      MapSet.union(path, acc)
+    paths = walk(graph, queue, visited, desti, [])
+
+    minimum =
+      paths
+      |> Enum.map(fn path -> score(path) end)
+      |> Enum.min()
+
+    paths
+    |> then(fn paths ->
+      paths
+      |> Enum.count()
+      |> IO.inspect(label: "Total paths")
+
+      paths
+      |> Enum.map(fn path -> score(path) end)
+      |> IO.inspect(label: "Path scores")
+
+      paths
     end)
+    |> Enum.filter(fn path -> score(path) == minimum end)
+    |> Enum.flat_map(fn path ->
+      Enum.map(path, fn {cord, _} -> cord end)
+    end)
+    |> MapSet.new()
     |> MapSet.size()
+  end
+
+  def walk(graph, queue, visited, desti, paths) do
+    # IO.inspect(visited)
+
+    case PriorityQueue.pop(queue) do
+      {{:value, {pos, direction, path}}, queue} ->
+        current_path = [{pos, direction} | path]
+        path_key = {pos, direction}
+
+        if pos == desti do
+          walk(graph, queue, visited, desti, [Enum.reverse(current_path) | paths])
+        else
+          # if MapSet.member?(visited, path_key) do
+          #   walk(graph, queue, visited, desti, paths)
+          # else
+          visited = MapSet.put(visited, path_key)
+
+          queue =
+            graph
+            |> Graph.out_neighbors(pos)
+            |> Enum.reject(fn npos ->
+              MapSet.member?(visited, {npos, direction(pos, npos)})
+            end)
+            |> Enum.reduce(queue, fn npos, acc ->
+              PriorityQueue.push(
+                acc,
+                {npos, direction(pos, npos), current_path},
+                0
+              )
+            end)
+
+          walk(graph, queue, visited, desti, paths)
+          # end
+        end
+
+      {:empty, _} ->
+        paths
+    end
+  end
+
+  def direction({fx, fy}, {tx, ty}) do
+    case {tx - fx, ty - fy} do
+      {1, 0} -> :right
+      {-1, 0} -> :left
+      {0, 1} -> :down
+      {0, -1} -> :up
+    end
+  end
+
+  defmemo score(path) do
+    path
+    |> Stream.chunk_every(2, 1, :discard)
+    |> Stream.map(fn [{_, fd}, {_, td}] ->
+      if fd == td do
+        1
+      else
+        1001
+      end
+    end)
+    |> Enum.sum()
   end
 
   def to_graph(map) do
     map
-    |> Enum.reduce(Graph.new(type: :directed), fn {pos, char}, g ->
+    |> Enum.reduce(Graph.new(type: :undirected), fn {pos, char}, g ->
       if char == "." do
         pos
         |> neighbors()
@@ -110,7 +131,8 @@ defmodule Aoc.Day16 do
         |> Enum.reduce(Graph.add_vertex(g, pos), fn npos, gg ->
           gg
           |> Graph.add_edge(pos, npos)
-          |> Graph.add_edge(npos, pos)
+
+          # |> Graph.add_edge(npos, pos)
         end)
       else
         g
