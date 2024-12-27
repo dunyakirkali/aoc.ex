483a3dababac594db1b3ff95d55d8511df1a523a
Dunya Kirkali <dunyakirkali@gmail.com>
2024-12-27 11:55:38 +0100

    Bump giraffe

--- 2024/aoc/mix.exs
+++ 2024/aoc/mix.exs
@@ -38,7 +38,7 @@ defmodule Aoc.MixProject do
       {:cll, "~> 0.2.0"},
       {:nx, "~> 0.5"},
       {:math, "~> 0.7.0"},
-      {:giraffe, "~> 0.1.0"}
+      {:giraffe, "~> 0.1.1"}
     ]
   end
 end
