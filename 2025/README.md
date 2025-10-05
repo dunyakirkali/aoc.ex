a90c5dc08a2cc0975baf4e8eaef68a325e648e9e
Dunya Kirkali <dunya@backbase.com>
2025-10-05 20:36:48 +0200

    Add 2025

---
+++ 2025/README.md
@@ -0,0 +1,4 @@
+# 2025
+
+I really should stop using [libgraph](https://github.com/bitwalker/libgraph).
+Instead just use [digraph](https://www.erlang.org/doc/apps/stdlib/digraph.html).
---
+++ 2025/aoc/mix.exs
@@ -0,0 +1,42 @@
+defmodule Aoc.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :aoc,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:nimble_parsec, "~> 1.4.2"},
+      # {:memoize, "~> 1.4.3"},
+      # {:benchee, "~> 1.3.1"},
+      # {:benchee_html, "~> 1.0.1", only: [:dev, :test]},
+      # {:printex, "~> 1.1.0"},
+      # {:distance, "~> 1.1.2"},
+      # {:poison, "~> 6.0.0"},
+      # {:comb, git: "https://github.com/tallakt/comb.git"},
+      # {:tensor, "~> 2.0"},
+      # {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      # {:exprof, "~> 0.2.0"},
+      # {:flow, "~> 1.2.4"},
+      # {:deque, "~> 1.2.0"},
+      # {:cll, "~> 0.2.0"},
+      # {:nx, "~> 0.5"},
+      # {:math, "~> 0.7.0"}
+    ]
+  end
+end
