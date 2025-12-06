d0c870d52c7d96027289bb8c8238e9f1b4d28b9c
Dunya Kirkali <dunyakirkali@gmail.com>
2025-12-06 07:41:28 +0100

    2025 Day 6 Part 2
    2025 Day 6 Part 2

--- 2025/aoc/lib/aoc/day6.ex
+++ 2025/aoc/lib/aoc/day6.ex
@@ -1,11 +1,9 @@
 defmodule Aoc.Day6 do
   @doc """
-      iex> "priv/day6/example.txt" |> Aoc.Day6.input() |> Aoc.Day6.part1()
+      iex> "priv/day6/example.txt" |> Aoc.Day6.input_part1() |> Aoc.Day6.part1()
       4277556
   """
   def part1({nums, cals}) do
-    {nums, cals}
-
     nums
     |> Enum.map(fn line ->
       line
@@ -28,103 +26,96 @@ defmodule Aoc.Day6 do
   end
 
   @doc """
-      iex> "priv/day6/example.txt" |> Aoc.Day6.input() |> Aoc.Day6.part2()
+      iex> "priv/day6/example.txt" |> Aoc.Day6.input_part2() |> Aoc.Day6.part2()
       3263827
+
+      iex> "priv/day6/example2.txt" |> Aoc.Day6.input_part2() |> Aoc.Day6.part2()
+      706262540
   """
-  def part2({nums, cals}) do
-    {nums, cals}
+  def part2({nums, bottom_row}) do
+    width =
+      [bottom_row | nums]
+      |> Enum.map(&String.length/1)
+      |> Enum.max()
 
-    len = length(cals)
+    rows =
+      nums
+      |> Enum.map(&String.pad_trailing(&1, width))
+      |> Enum.map(&String.graphemes/1)
 
-    nums
-    |> Enum.map(fn line ->
-      line
+    bottom =
+      bottom_row
+      |> String.pad_trailing(width)
       |> String.graphemes()
-      |> Enum.map(fn x ->
-        case x do
-          " " -> 0
-          v -> String.to_integer(v)
-        end
-      end)
-      |> Enum.chunk_every(len)
-      |> pad_with_zeros()
-      |> Enum.map(fn list ->
-        Enum.slice(list, 0, length(list) - 1)
-      end)
-    end)
-    |> combine
-    |> Enum.with_index()
-    |> Enum.map(fn {tuple, i} ->
-      tuple
-      |> IO.inspect()
-      |> Enum.map(&drop_trailing_zeros/1)
-      |> Enum.reduce(fn x, acc ->
-        case Enum.at(cals, i) do
-          "*" -> acc * x
-          "+" -> acc + x
-        end
-      end)
-    end)
-    |> Enum.sum()
-  end
 
-  def drop_trailing_zeros(n) when is_integer(n) do
-    n
-    |> Integer.to_string()
-    |> String.trim_trailing("0")
-    |> case do
-      "" -> "0"
-      s -> s
-    end
-    |> String.to_integer()
-  end
-
-  def combine(rows) do
-    # rows is a list of lists of triples
-
-    num_cols =
-      rows
-      |> List.first()
-      |> length()
-
-    num_positions =
-      rows
-      |> List.first()
-      |> hd()
-      |> length()
-
-    for col <- 0..(num_cols - 1) do
-      for pos <- 0..(num_positions - 1) do
-        rows
-        |> Enum.map(fn row -> Enum.at(row, col) |> Enum.at(pos) end)
-        |> Enum.join()
-        |> String.to_integer()
+    cols =
+      for col <- 0..(width - 1) do
+        col_chars = Enum.map(rows, fn r -> Enum.at(r, col) end)
+        op_char = Enum.at(bottom, col)
+        {col_chars, op_char}
       end
-    end
+
+    sep? = fn {col_chars, _op} -> Enum.all?(col_chars, &(&1 == " ")) end
+
+    groups =
+      cols
+      |> Enum.chunk_by(sep?)
+      |> Enum.reject(fn group -> sep?.(hd(group)) end)
+      |> Enum.reverse()
+
+    results =
+      Enum.map(groups, fn group ->
+        op =
+          group
+          |> Enum.reverse()
+          |> Enum.map(fn {_chars, op_char} -> op_char end)
+          |> Enum.find("+", fn ch -> ch != " " end)
+
+        nums_in_group =
+          group
+          |> Enum.map(fn {col_chars, _op_char} ->
+            digits = Enum.filter(col_chars, &(&1 =~ ~r/^\d$/))
+
+            case digits do
+              [] -> nil
+              _ -> digits |> Enum.join() |> String.to_integer()
+            end
+          end)
+          |> Enum.filter(& &1)
+
+        case op do
+          "+" -> Enum.sum(nums_in_group)
+          "*" -> Enum.reduce(nums_in_group, 1, &*/2)
+        end
+      end)
+
+    Enum.sum(results)
   end
 
-  def pad_with_zeros(lists) do
-    max_len = lists |> Enum.map(&length/1) |> Enum.max()
-
-    Enum.map(lists, fn list ->
-      zeros_to_add = max_len - length(list)
-      list ++ List.duplicate(0, zeros_to_add)
-    end)
-  end
-
-  def input(filename) do
-    list =
+  def input_part1(filename) do
+    lines =
       filename
       |> File.read!()
       |> String.split("\n", trim: true)
 
     cals =
-      List.last(list)
+      List.last(lines)
       |> String.split(" ", trim: true)
 
     nums =
-      Enum.slice(list, 0, length(list) - 1)
+      Enum.slice(lines, 0, length(lines) - 1)
 
     {nums, cals}
+  end
+
+  def input_part2(filename) do
+    lines =
+      filename
+      |> File.read!()
+      |> String.split("\n", trim: true)
+
+    bottom_row = List.last(lines)
+    nums = Enum.slice(lines, 0, length(lines) - 1)
+    {nums, bottom_row}
   end
 end
--- 2025/aoc/test/aoc/day6_test.exs
+++ 2025/aoc/test/aoc/day6_test.exs
@@ -3,13 +3,13 @@ defmodule Day6Test do
   doctest Aoc.Day6
 
   test "Part 1" do
-    input = Aoc.Day6.input("priv/day6/input.txt")
+    input = Aoc.Day6.input_part1("priv/day6/input.txt")
     assert Aoc.Day6.part1(input) == 5_784_380_717_354
   end
 
   @tag timeout: :infinity
   test "Part 2" do
-    input = Aoc.Day6.input("priv/day6/input.txt")
-    assert Aoc.Day6.part2(input) == 7_996_215_336_396
+    input = Aoc.Day6.input_part2("priv/day6/input.txt")
+    assert Aoc.Day6.part2(input) == 7_996_218_225_744
   end
 end
